9abec754b08c8bf5fffc0fac1c23b666a030fa66
James Farrell <info@jamespfarrell.com>
2020-03-27 21:17:29 +0100

    Working with many to many organisations and projects

--- resolvers/user-resolver.ts
+++ resolvers/user-resolver.ts
@@ -8,7 +8,7 @@ import {
   Ctx,
   Int
 } from 'type-graphql'
-import { Repository } from 'typeorm'
+import { Repository, In } from 'typeorm'
 import { InjectRepository } from 'typeorm-typedi-extensions'
 
 import { Recipe } from '../entities/recipe'
@@ -18,11 +18,14 @@ import { RecipeInput } from './types/recipe-input'
 import { Context } from '../index'
 import { RateInput } from './types/rate-input'
 import { OrganisationUser } from '../entities/organisationUser'
+import { Organisation } from '../entities/organisation'
 
 @Resolver(of => User)
 export class UserResolver {
   constructor (
     @InjectRepository(User) private readonly userRepository: Repository<User>,
+    @InjectRepository(Organisation)
+    private readonly organisationRepository: Repository<Organisation>,
     @InjectRepository(OrganisationUser)
     private readonly organisationUserRepository: Repository<OrganisationUser>
   ) {}
@@ -42,6 +45,20 @@ export class UserResolver {
     return this.organisationUserRepository.find({
       cache: 1000,
       where: { authorId: user.id }
+    })
+  }
+
+  @FieldResolver()
+  async organisations (@Root() user: User) {
+    const organisationUsers = await this.organisationUserRepository.find({
+      cache: 1000,
+      where: { userId: user.id }
+    })
+
+    const organisationUserIds = organisationUsers.map(o => o.id)
+    return await this.organisationRepository.find({
+      cache: 1000,
+      where: { organisationUserId: In(organisationUserIds) }
     })
   }
 }
