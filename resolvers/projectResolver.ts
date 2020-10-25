716ac854858ec5242d489ef34325ce7725d9cffe
Mateo Daza <mateodaza@gmail.com>
2020-10-25 07:38:58 -0500

    removed auth check on project creation

--- resolvers/projectResolver.ts
+++ resolvers/projectResolver.ts
@@ -166,24 +166,28 @@ export class ProjectResolver {
     @Ctx() ctx: MyContext,
     @PubSub() pubSub: PubSubEngine
   ): Promise<Project> {
-    //check this
-    if (!ctx.req.user) {
-      console.log(`access denied : ${JSON.stringify(ctx.req.user, null, 2)}`)
-      throw new Error('Access denied')
-      // return undefined
-    }
-    console.log(`Add project user email: ${ctx.req.user.email}`)
-    if (!ctx.req.user) {
-      console.log(`access denied : ${JSON.stringify(ctx.req.user, null, 2)}`)
-      throw new Error('Access denied')
-      // return undefined
-    }
-    if (
-      await this.userPermissions.mayAddProjectToOrganisation(
-        ctx.req.user.email,
-        projectInput.organisationId
-      )
-    ) {
+    // AUTH CHECK
+
+    // if (!ctx.req.user) {
+    //   console.log(`access denied : ${JSON.stringify(ctx.req.user, null, 2)}`)
+    //   throw new Error('Access denied')
+    //   // return undefined
+    // }
+    // console.log(`Add project user email: ${ctx.req.user.email}`)
+    // if (!ctx.req.user) {
+    //   console.log(`access denied : ${JSON.stringify(ctx.req.user, null, 2)}`)
+    //   throw new Error('Access denied')
+    //   // return undefined
+    // }
+
+    // THIS CHECKS PERMISSIONS ON A BASIC AUTH FLOW
+
+    // if (
+    //   await this.userPermissions.mayAddProjectToOrganisation(
+    //     ctx.req.user.email,
+    //     projectInput.organisationId
+    //   )
+    // ) {
 
       const categories = await Promise.all(projectInput.categories ?
         projectInput.categories.map(async category => {
@@ -223,11 +227,11 @@ export class ProjectResolver {
       await pubSub.publish('NOTIFICATIONS', payload)
 
       return newProject
-    } else {
-      throw new Error(
-        'User does not have the right to create a project for this organisation'
-      )
-    }
+    // } else {
+    //   throw new Error(
+    //     'User does not have the right to create a project for this organisation'
+    //   )
+    // }
 
     // await pubSub.publish('NOTIFICATIONS', payload)
 
