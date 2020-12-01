534da8853f17e85c53d4a9a29d7f3ff882560a3c
ctorres@dexfreight.io <ctorres@dexfreight.io>
2020-11-30 19:03:12 -0500

    Fix Projects and Edit Project

--- resolvers/projectResolver.ts
+++ resolvers/projectResolver.ts
@@ -95,7 +95,7 @@ class GetProjectsArgs {
   category: string
 
   @Field({ nullable: true })
-  admin: number
+  admin?: number
 }
 
 @Service()
@@ -141,7 +141,7 @@ export class ProjectResolver {
 
   @Query(returns => [Project])
   async projects (@Args() { take, skip, admin }: GetProjectsArgs): Promise<Project[]> {
-    return this.projectRepository.find({
+    return !admin? this.projectRepository.find({ take, skip }) : this.projectRepository.find({
       where: { admin },
       take, skip
     })
@@ -197,6 +197,20 @@ export class ProjectResolver {
 
     for (const field in newProjectData)
       project[field] = newProjectData[field];
+
+    if (newProjectData.categories) {
+      const categoriesPromise = newProjectData.categories.map(async category => {
+        let [c] = await this.categoryRepository.find({ name: category });
+        if (c === undefined) {
+          c = new Category();
+          c.name = category;
+        }
+        return c;
+      })
+
+      const categories = await Promise.all(categoriesPromise)
+      project.categories = categories;
+    }
 
     await project.save();
 
