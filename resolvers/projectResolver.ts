d9774b10888eecdace1df96adfd85427e3e15875
James Farrell <info@jamespfarrell.com>
2020-12-03 00:57:16 +0100

    remove reaction code as its not used

--- resolvers/projectResolver.ts
+++ resolvers/projectResolver.ts
@@ -413,35 +413,6 @@ export class ProjectResolver {
     return ProjectUpdate.save(update);
   }
 
-  @Mutation(returns => Boolean)
-  async toggleReaction (
-    @Arg('updateId') updateId: number,
-    @Arg('reaction') reaction: PROJECT_UPDATE_REACTIONS = 'heart',
-    @Ctx() { req: { user } }: MyContext,
-    @PubSub() pubSub: PubSubEngine
-  ): Promise<boolean> {
-    if (!user) throw new Error('Authentication required.')
-
-    const update = await ProjectUpdate.findOne({ id: updateId });
-    if (!update) throw new Error('Update not found.');
-    
-    const currentReaction = await ProjectUpdateReactions.findOne({ userId: user.userId });
-    
-    await ProjectUpdateReactions.delete({ userId: user.userId });
-
-    if (currentReaction && currentReaction.reaction === reaction) return false;
-
-    const newReaction = await ProjectUpdateReactions.create({
-      userId: user.userId,
-      projectUpdateId: update.id,
-      reaction
-    })
-
-    await ProjectUpdateReactions.save(newReaction)
-
-    return true;
-  }
-
   @Query(returns => [GetProjectUpdatesResult])
   async getProjectUpdates (
     @Arg('projectId') projectId: number,
