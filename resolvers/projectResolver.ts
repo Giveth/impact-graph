8ab317857d2895e2016a614e2c5f31441ddb4fe3
ctorres@dexfreight.io <ctorres@dexfreight.io>
2021-01-04 22:41:33 -0500

    Fix ToggleProject Reaction

--- resolvers/projectResolver.ts
+++ resolvers/projectResolver.ts
@@ -470,8 +470,19 @@ export class ProjectResolver {
   ): Promise<boolean> {
     if (!user) throw new Error('Authentication required.')
 
-    const update = await ProjectUpdate.findOne({ projectId, isMain: true });
-    if (!update) throw new Error('Update not found.');
+    let update = await ProjectUpdate.findOne({ projectId, isMain: true });
+    if (!update) {
+      let project = await Project.findOne({ id: projectId });
+      
+      update = await ProjectUpdate.save(await ProjectUpdate.create({
+        userId: +project?.admin,
+        projectId: projectId,
+        content: "",
+        title: "",
+        createdAt: new Date(),
+        isMain: true
+      }));
+    }
     
     const currentReaction = await ProjectUpdateReactions.findOne({ projectUpdateId: update.id, userId: user.userId });
     
