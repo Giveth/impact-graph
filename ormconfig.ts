40fa18a7c64b2909222607cbe5dffba173f29868
James Farrell <info@jamespfarrell.com>
2021-02-06 19:43:46 +0100

    debug build

--- ormconfig.ts
+++ ormconfig.ts
@@ -5,9 +5,6 @@ import { ConnectionOptions } from 'typeorm';
 import { entities } from './entities/entities';
 dotenv.config({ path: path.resolve(__dirname, `./config/${process.env.NODE_ENV||''}.env`) });
 
-console.log(`env file ---> : ${path.resolve(__dirname)}/config/${process.env.NODE_ENV||''}.env`)
-console.log(`process.env.NODE_ENV ---> : ${process.env.NODE_ENV}`)
-console.log(`process.env.TYPEORM_DATABASE_USER ---> : ${process.env.TYPEORM_DATABASE_USER}`)
 const ormConfig: ConnectionOptions = {
   type: 'postgres',
   host: process.env.TYPEORM_DATABASE_HOST,
