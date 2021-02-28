ada808e53e6d573d71caad994edd8486950ffc42
James Farrell <info@jamespfarrell.com>
2021-02-28 12:27:52 +0100

    Update migrations to get them to run

--- migration/1614082287595-ProjectStatusRelationship.ts
+++ migration/1614082287595-ProjectStatusRelationship.ts
@@ -3,8 +3,7 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class ProjectStatusRelationship1614082287595 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-         //wtf?
-         await queryRunner.query(`ALTER TABLE "project" DROP COLUMN "projectId"`);
+         //wtf? await queryRunner.query(`ALTER TABLE "project" DROP COLUMN "projectId"`);
          await queryRunner.query(`ALTER TABLE "project" ADD CONSTRAINT "FK_b6d55aff9b16e061712260da686" FOREIGN KEY ("statusId") REFERENCES "project_status"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
          
     }
