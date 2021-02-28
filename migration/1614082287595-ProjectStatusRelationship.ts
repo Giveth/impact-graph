979bc29f35b59354e3c7088f94ea2a8a8f3b504b
James Farrell <info@jamespfarrell.com>
2021-02-28 14:07:07 +0100

    debug migration failure

--- migration/1614082287595-ProjectStatusRelationship.ts
+++ migration/1614082287595-ProjectStatusRelationship.ts
@@ -4,6 +4,8 @@ export class ProjectStatusRelationship1614082287595 implements MigrationInterfac
 
     public async up(queryRunner: QueryRunner): Promise<void> {
          //wtf? await queryRunner.query(`ALTER TABLE "project" DROP COLUMN "projectId"`);
+         await queryRunner.query(`UPDATE "project" SET "statusId" = 5`);
+
          await queryRunner.query(`ALTER TABLE "project" ADD CONSTRAINT "FK_b6d55aff9b16e061712260da686" FOREIGN KEY ("statusId") REFERENCES "project_status"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
          
     }
