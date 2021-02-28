ac206e17302046f72ef722ca83a6956c3f7a1824
James Farrell <info@jamespfarrell.com>
2021-02-28 12:35:10 +0100

    remove deplicate migration

--- migration/1614358574603-TransactionNetworkId.ts
+++ migration/1614358574603-TransactionNetworkId.ts
@@ -5,11 +5,9 @@ export class TransactionNetworkId1614358574603 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "donation" ADD "transactionNetworkId" integer NOT NULL constraint default_transactionNetworkId default (1)`);
-        await queryRunner.query(`ALTER TABLE "project" ADD CONSTRAINT "FK_b6d55aff9b16e061712260da686" FOREIGN KEY ("statusId") REFERENCES "project_status"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "project" DROP CONSTRAINT "FK_b6d55aff9b16e061712260da686"`);
         await queryRunner.query(`ALTER TABLE "donation" DROP COLUMN "transactionNetworkId"`);
     }
 
