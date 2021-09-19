c591d1c61ca8087086a71dd4c36e41512bb16296
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2021-09-19 22:48:59 +0430

    Fix returning projects on query user
    related to #66

--- entities/user.ts
+++ entities/user.ts
@@ -96,7 +96,7 @@ export class User extends BaseEntity {
   )
   organisations: Organisation[]
 
-  @Field(type => Project)
+  @Field(type => [Project])
   @ManyToMany(
     type => Project,
     project => project.users
