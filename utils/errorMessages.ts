72675aeede9ad6294b506856a5b6f0f63d3eb77c
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2021-09-20 15:58:22 +0430

    Change error message for selecting more than 5 categories

--- utils/errorMessages.ts
+++ utils/errorMessages.ts
@@ -1,5 +1,5 @@
 export const errorMessages = {
-  CATEGORIES_LENGTH_SHOULD_NOT_BE_MORE_THAN_FIVE: 'Categories should not be more than 5',
+  CATEGORIES_LENGTH_SHOULD_NOT_BE_MORE_THAN_FIVE: 'Please select no more than 5 categories',
   YOU_ARE_NOT_THE_OWNER_OF_PROJECT: 'You are not the owner of this project.',
   PROJECT_NOT_FOUND: 'Project not found.',
   USER_NOT_FOUND: 'User not found.'
