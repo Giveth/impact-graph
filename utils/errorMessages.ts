f66e3338654b9c3a5961660ae8713a9871c657db
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2021-10-30 19:06:02 +0330

    Throw error if both firstName and lastName are empty in updateProfile mutation

--- utils/errorMessages.ts
+++ utils/errorMessages.ts
@@ -4,6 +4,7 @@ export const errorMessages = {
   PROJECT_NOT_FOUND: 'Project not found.',
   USER_NOT_FOUND: 'User not found.',
   INVALID_WALLET_ADDRESS: 'Eth address not valid',
+  BOTH_FIRST_NAME_AND_LAST_NAME_CANT_BE_EMPTY: 'Both firstName and lastName cant be empty',
   PROJECT_WITH_THIS_TITLE_EXISTS: 'There is a project with this title, please use another title',
   INVALID_PROJECT_TITLE: 'Your project name isn\'t valid, please only use letters and numbers'
 }
