5227e7982f716de29bcae8fac6e392d78fc98eed
Amin Latifi <a.latifi.al@gmail.com>
2021-10-31 15:26:38 +0330

    Fixed linting issues

--- utils/validators/projectValidator.ts
+++ utils/validators/projectValidator.ts
@@ -1,95 +1,108 @@
-import { getProvider, NETWORK_IDS } from '../../provider'
-import { Project } from '../../entities/project'
-import Web3 from 'web3'
-import { errorMessages } from '../errorMessages'
+import { getProvider, NETWORK_IDS } from '../../provider';
+import { Project } from '../../entities/project';
+import Web3 from 'web3';
+import { errorMessages } from '../errorMessages';
 
 export function isWalletAddressValid(address) {
   return Boolean(
-    address && address.length === 42 && Web3.utils.isAddress(address)
-  )
+    address && address.length === 42 && Web3.utils.isAddress(address),
+  );
 }
 
 export const validateProjectWalletAddress = async (
-  walletAddress: string
+  walletAddress: string,
 ): Promise<boolean> => {
   if (!isWalletAddressValid(walletAddress)) {
-    throw new Error(errorMessages.INVALID_WALLET_ADDRESS)
+    throw new Error(errorMessages.INVALID_WALLET_ADDRESS);
   }
   const isSmartContractWallet = await isWalletAddressSmartContract(
-    walletAddress
-  )
+    walletAddress,
+  );
   if (isSmartContractWallet) {
     throw new Error(
-      `Eth address ${walletAddress} is a smart contract. We do not support smart contract wallets at this time because we use multiple blockchains, and there is a risk of your losing donations.`
-    )
+      `Eth address ${walletAddress} is a smart contract. We do not support smart contract wallets at this time because we use multiple blockchains, and there is a risk of your losing donations.`,
+    );
   }
   const projectWithAddress = await Project.createQueryBuilder('project')
-    .where(`lower("walletAddress")=lower(: walletAddress )`,{
-      walletAddress
+    .where(`lower("walletAddress")=lower(: walletAddress )`, {
+      walletAddress,
     })
-    .getOne()
+    .getOne();
   if (projectWithAddress) {
     throw new Error(
-      `Eth address ${walletAddress} is already being used for a project`
-    )
+      `Eth address ${walletAddress} is already being used for a project`,
+    );
   }
-  return true
-}
+  return true;
+};
 
 const titleReplacerRegex = /^\s+|\s+$|\s+(?=\s)/g;
 
-export const validateProjectTitleForEdit = async (title:string, projectId: number)=>{
-  const project  = await Project.findOne(projectId)
-  if (getSimilarTitleInProjectsRegex(project?.title as string).test(title.replace(titleReplacerRegex, '')) ){
+export const validateProjectTitleForEdit = async (
+  title: string,
+  projectId: number,
+) => {
+  const project = await Project.findOne(projectId);
+  if (
+    getSimilarTitleInProjectsRegex(project?.title as string).test(
+      title.replace(titleReplacerRegex, ''),
+    )
+  ) {
     // If the new title of project is similar to older one , we dont call validateProjectTitle
     return true;
   }
-  return  validateProjectTitle(title)
-}
+  return validateProjectTitle(title);
+};
 
-
-export const getSimilarTitleInProjectsRegex = (title:string ):RegExp => {
+export const getSimilarTitleInProjectsRegex = (title: string): RegExp => {
   return new RegExp(`^\\s*${title.replace(titleReplacerRegex, '')}\\s*$`, 'i');
 };
 
-export const validateProjectTitle = async (title: string) :Promise <boolean>=> {
-  const isTitleValid = /^\w+$/.test(title.replace(/\s/g, ''))
-  if (!isTitleValid){
-    throw new Error(errorMessages.INVALID_PROJECT_TITLE)
+export const validateProjectTitle = async (title: string): Promise<boolean> => {
+  const isTitleValid = /^\w+$/.test(title.replace(/\s/g, ''));
+  if (!isTitleValid) {
+    throw new Error(errorMessages.INVALID_PROJECT_TITLE);
   }
-  const regex = getSimilarTitleInProjectsRegex(title)
-  console.log('regexSource', { title, regex })
+  const regex = getSimilarTitleInProjectsRegex(title);
+  console.log('regexSource', { title, regex });
   const projectWithThisTitle = await Project.query(
-    `SELECT title , REGEXP_MATCHES(title, '${regex.source}','i') FROM project;`)
-  console.log('validateProjectTitle projectWithThisTitle', projectWithThisTitle)
+    `SELECT title , REGEXP_MATCHES(title, '${regex.source}','i') FROM project;`,
+  );
+  console.log(
+    'validateProjectTitle projectWithThisTitle',
+    projectWithThisTitle,
+  );
 
   if (projectWithThisTitle.length > 0) {
-    console.log('validateProjectTitle projectWithThisTitle', projectWithThisTitle)
-    throw new Error(errorMessages.PROJECT_WITH_THIS_TITLE_EXISTS)
+    console.log(
+      'validateProjectTitle projectWithThisTitle',
+      projectWithThisTitle,
+    );
+    throw new Error(errorMessages.PROJECT_WITH_THIS_TITLE_EXISTS);
   }
   return true;
-}
+};
 
 export const isWalletAddressSmartContract = async (
-  address: string
+  address: string,
 ): Promise<boolean> => {
-  const mainnetProvider = getProvider(NETWORK_IDS.MAIN_NET)
-  const xdaiProvider = getProvider(NETWORK_IDS.XDAI)
-  const isSmartContractMainnet = isSmartContract(mainnetProvider)
-  const isSmartContractXDai = isSmartContract(xdaiProvider)
-  const isContractPromises: any = []
-  isContractPromises.push(isSmartContractMainnet(address))
-  isContractPromises.push(isSmartContractXDai(address))
+  const mainnetProvider = getProvider(NETWORK_IDS.MAIN_NET);
+  const xdaiProvider = getProvider(NETWORK_IDS.XDAI);
+  const isSmartContractMainnet = isSmartContract(mainnetProvider);
+  const isSmartContractXDai = isSmartContract(xdaiProvider);
+  const isContractPromises: any = [];
+  isContractPromises.push(isSmartContractMainnet(address));
+  isContractPromises.push(isSmartContractXDai(address));
 
   return Promise.all(isContractPromises).then(promises => {
-    const [isSmartContractOnMainnet, isSmartContractOnXDai] = promises
-    return Boolean(isSmartContractOnMainnet || isSmartContractOnXDai)
-  })
-}
+    const [isSmartContractOnMainnet, isSmartContractOnXDai] = promises;
+    return Boolean(isSmartContractOnMainnet || isSmartContractOnXDai);
+  });
+};
 
 function isSmartContract(provider) {
   return async projectWalletAddress => {
-    const code = await provider.getCode(projectWalletAddress)
-    return code !== '0x'
-  }
+    const code = await provider.getCode(projectWalletAddress);
+    return code !== '0x';
+  };
 }
