39befffb263f3ff2ee5a30a12526aa9396639479
Carlos Quintero <carlos.quintero096@gmail.com>
2022-02-02 00:36:51 -0500

    add donations by donor query tests

--- src/utils/errorMessages.ts
+++ src/utils/errorMessages.ts
@@ -6,6 +6,8 @@ export const errorMessages = {
   YOU_ARE_NOT_THE_OWNER_OF_PROJECT: 'You are not the owner of this project.',
   PROJECT_NOT_FOUND: 'Project not found.',
   DONATION_NOT_FOUND: 'donation not found',
+  DONATION_VIEWING_LOGIN_REQUIRED:
+    'You must be signed-in in order to register project donations',
   TRANSACTION_NOT_FOUND: 'Transaction not found.',
   TRANSACTION_FROM_ADDRESS_IS_DIFFERENT_FROM_SENT_FROM_ADDRESS:
     'FromAddress of Transaction is different from sent fromAddress',
