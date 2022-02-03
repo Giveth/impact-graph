af1212f4df5260428da082fcf8d8f37b20d48faf
Carlos Quintero <carlos.quintero096@gmail.com>
2022-02-03 17:10:01 -0500

    set segment notified as true before typeorm sync runs

--- migration/1643891610952-setSegmentNotifiedForOlderDonations.ts
+++ migration/1643891610952-setSegmentNotifiedForOlderDonations.ts
@@ -3,30 +3,11 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class setSegmentNotifiedForOlderDonations1643891610952
   implements MigrationInterface
 {
-  // Update statements don't have if exists clause, we have to run another query or create a pg function
-  public async up(queryRunner: QueryRunner): Promise<void> {
-    const donationTableExists = await queryRunner.query(`
-      SELECT EXISTS (
-        SELECT FROM pg_tables
-        WHERE  schemaname = 'public'
-        AND    tablename  = 'donation'
-      );
-    `);
-    if (donationTableExists[0].exists) {
-      await queryRunner.query(`UPDATE donation SET "segmentNotified" = true`);
-    }
+  async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE IF EXISTS  "donation" ADD  "segmentNotified" boolean DEFAULT true`,
+    );
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {
-    const donationTableExists = await queryRunner.query(`
-      SELECT EXISTS (
-        SELECT FROM pg_tables
-        WHERE  schemaname = 'public'
-        AND    tablename  = 'donation'
-      );
-    `);
-    if (donationTableExists[0].exists) {
-      await queryRunner.query(`UPDATE donation SET "segmentNotified" = false`);
-    }
-  }
+  async down(queryRunner: QueryRunner): Promise<void> {}
 }
