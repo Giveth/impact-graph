cfdaac6474676c5500eeac1e5bcddb3ff1c9323a
Carlos Quintero <carlos.quintero096@gmail.com>
2022-02-03 08:51:09 -0500

    validate donation table existance before updating migration

--- migration/1643891610952-setSegmentNotifiedForOlderDonations.ts
+++ migration/1643891610952-setSegmentNotifiedForOlderDonations.ts
@@ -3,15 +3,30 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class setSegmentNotifiedForOlderDonations1643891610952
   implements MigrationInterface
 {
+  // Update statements don't have if exists clause, we have to run another query or create a pg function
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `UPDATE donation SET "segmentNotified" = true`,
-    );
+    const donationTableExists = await queryRunner.query(`
+      SELECT EXISTS (
+        SELECT FROM pg_tables
+        WHERE  schemaname = 'public'
+        AND    tablename  = 'donation'
+      );
+    `);
+    if (donationTableExists[0].exists) {
+      await queryRunner.query(`UPDATE donation SET "segmentNotified" = true`);
+    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `UPDATE donation SET "segmentNotified" = false`,
-    );
+    const donationTableExists = await queryRunner.query(`
+      SELECT EXISTS (
+        SELECT FROM pg_tables
+        WHERE  schemaname = 'public'
+        AND    tablename  = 'donation'
+      );
+    `);
+    if (donationTableExists[0].exists) {
+      await queryRunner.query(`UPDATE donation SET "segmentNotified" = false`);
+    }
   }
 }
