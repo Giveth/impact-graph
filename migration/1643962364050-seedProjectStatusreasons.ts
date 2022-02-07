1380da9ea168f01fcc92316e249d0ec833243519
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2022-02-07 05:21:19 +0330

    Add comment to seedProjectStatusReasons migration

--- migration/1643962364050-seedProjectStatusreasons.ts
+++ migration/1643962364050-seedProjectStatusreasons.ts
@@ -5,6 +5,7 @@ export class seedProjectStatusreasons1643962364050
   implements MigrationInterface
 {
   async up(queryRunner: QueryRunner): Promise<void> {
+    // https://github.com/Giveth/giveth-io-typescript/issues/84#issuecomment-1016941722
     await queryRunner.query(`INSERT INTO public.project_status_reason ("statusId", description ) VALUES 
                     (${ProjStatus.deactive},'The project has completed its goals!'),
                     (${ProjStatus.deactive},'The project is no longer in need of funding.'),
