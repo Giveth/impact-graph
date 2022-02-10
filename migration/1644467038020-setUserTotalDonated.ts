4df4de0a876c47e6ce7fcb732f18d43d289731b5
Carlos Quintero <carlos.quintero096@gmail.com>
2022-02-10 01:37:05 -0500

    fix user profile migration files

--- migration/1644467038020-setUserTotalDonated.ts
+++ migration/1644467038020-setUserTotalDonated.ts
@@ -2,20 +2,27 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class setUserTotalDonated1644467038020 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE IF EXISTS "user" ADD "totalDonated" integer DEFAULT 0`,
-    );
+    // To run the update query SUM I should check table existance
+    // UPDATE clause doesn't have a IF EXISTS conditional
+    const userTableExists = await queryRunner.query(`
+      SELECT EXISTS (SELECT FROM pg_tables WHERE  schemaname = 'public' AND tablename = 'user');`);
 
-    await queryRunner.query(`
-      UPDATE "user"
-      SET "totalDonated" = d."totalDonationsDonated"
-      FROM (
-        SELECT "userId", SUM("valueUsd") AS "totalDonationsDonated"
-        FROM donation
-        GROUP BY "userId"
-      ) AS d
-      WHERE d."userId" = id
-    `);
+    if (userTableExists[0].exists) {
+      await queryRunner.query(
+        `ALTER TABLE "user" ADD "totalDonated" integer DEFAULT 0`,
+      );
+
+      await queryRunner.query(`
+        UPDATE "user"
+        SET "totalDonated" = d."totalDonationsDonated"
+        FROM (
+            SELECT "userId", SUM("valueUsd") AS "totalDonationsDonated"
+            FROM donation
+            GROUP BY "userId"
+        ) AS d
+        WHERE d."userId" = id
+      `);
+    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
--- migration/1644467298829-setUserTotalReceived.ts
+++ migration/1644467298829-setUserTotalReceived.ts
@@ -2,20 +2,27 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class setUserTotalReceived1644467298829 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE IF EXISTS "user" ADD "totalReceived" integer DEFAULT 0`,
-    );
+    // To run the update query SUM I should check table existance
+    // UPDATE clause doesn't have a IF EXISTS conditional
+    const userTableExists = await queryRunner.query(`
+      SELECT EXISTS (SELECT FROM pg_tables WHERE  schemaname = 'public' AND tablename = 'user');`);
 
-    await queryRunner.query(`
-      UPDATE "user"
-      SET "totalReceived" = p."totalProjectReceived"
-      FROM (
-        SELECT "admin", SUM("totalDonations") AS "totalProjectReceived"
-        FROM project
-        GROUP BY "admin"
-      ) AS p
-      WHERE p."admin" = CAST(id AS text)
-    `);
+    if (userTableExists[0].exists) {
+      await queryRunner.query(
+        `ALTER TABLE "user" ADD "totalReceived" integer DEFAULT 0`,
+      );
+
+      await queryRunner.query(`
+        UPDATE "user"
+        SET "totalReceived" = p."totalProjectReceived"
+        FROM (
+            SELECT "admin", SUM("totalDonations") AS "totalProjectReceived"
+            FROM project
+            GROUP BY "admin"
+        ) AS p
+        WHERE p."admin" = CAST(id AS text)
+      `);
+    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
