097a55a93c417c99d95e2ae4a60668df1ba61009
Carlos Quintero <carlos.quintero096@gmail.com>
2022-02-21 23:15:35 -0500

    add register input for profile image upload

--- src/user/register/RegisterInput.ts
+++ src/user/register/RegisterInput.ts
@@ -2,6 +2,7 @@ import { Length, IsEmail } from 'class-validator';
 import { Field, InputType } from 'type-graphql';
 import { IsEmailAlreadyExist } from './isEmailAlreadyExist';
 import { PasswordMixin } from '../../types/PasswordInput';
+import { GraphQLUpload, FileUpload } from 'graphql-upload';
 
 @InputType()
 export class RegisterInput extends PasswordMixin(class {}) {
@@ -17,4 +18,9 @@ export class RegisterInput extends PasswordMixin(class {}) {
   @IsEmail()
   @IsEmailAlreadyExist({ message: 'email already in use' })
   email: string;
+}
+
+export class ProfileImageInput {
+  @Field(type => GraphQLUpload, { nullable: false })
+  image: FileUpload;
 }
