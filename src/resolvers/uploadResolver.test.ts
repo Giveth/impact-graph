0f27aed2d8f0d898b938c7bb785d48703652e5e2
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2022-02-23 10:26:48 +0330

    Fix uploadResolver test cases

--- src/resolvers/uploadResolver.test.ts
+++ src/resolvers/uploadResolver.test.ts
@@ -42,9 +42,10 @@ function uploadTestCases() {
         JSON.stringify(e.response.data, null, 4),
       );
       // TODO currently I dont know how upload images to graphql mutation, so we got error, but we should fix this test case later and remove try-catch
-      assert.equal(
-        e.response.data.errors[0].message,
-        'Variable "$fileUpload" got invalid value { _readableState: { objectMode: false, highWaterMark: 65536, buffer: [Object], length: 0, pipes: [], flowing: null, ended: false, endEmitted: false, reading: false, sync: true, needReadable: false, emittedReadable: false, readableListening: false, resumeScheduled: false, errorEmitted: false, emitClose: true, autoDestroy: false, destroyed: false, errored: null, closed: false, closeEmitted: false, defaultEncoding: "utf8", awaitDrainWriters: null, multiAwaitDrain: false, readingMore: false, decoder: null, encoding: null }, _events: {}, _eventsCount: 1, path: "/Users/renjer/Documents/src/giveth/impact-graph/test/images/testImage.jpg", fd: null, flags: "r", mode: 438, end: null, autoClose: true, bytesRead: 0, closed: false } at "fileUpload.image"; Upload value invalid.',
+      assert.isTrue(
+        e.response.data.errors[0].message.includes(
+          'Variable "$fileUpload" got invalid value',
+        ),
       );
     }
   });
@@ -83,9 +84,10 @@ function uploadTestCases() {
         JSON.stringify(e.response.data, null, 4),
       );
       // TODO currently I dont know how upload images to graphql mutation, so we got error, but we should fix this test case later and remove try-catch
-      assert.equal(
-        e.response.data.errors[0].message,
-        'Variable "$fileUpload" got invalid value { _readableState: { objectMode: false, highWaterMark: 65536, buffer: [Object], length: 0, pipes: [], flowing: null, ended: false, endEmitted: false, reading: false, sync: true, needReadable: false, emittedReadable: false, readableListening: false, resumeScheduled: false, errorEmitted: false, emitClose: true, autoDestroy: false, destroyed: false, errored: null, closed: false, closeEmitted: false, defaultEncoding: "utf8", awaitDrainWriters: null, multiAwaitDrain: false, readingMore: false, decoder: null, encoding: null }, _events: {}, _eventsCount: 1, path: "/Users/renjer/Documents/src/giveth/impact-graph/test/images/testImage.jpg", fd: null, flags: "r", mode: 438, end: null, autoClose: true, bytesRead: 0, closed: false } at "fileUpload.image"; Upload value invalid.',
+      assert.isTrue(
+        e.response.data.errors[0].message.includes(
+          'Variable "$fileUpload" got invalid value',
+        ),
       );
     }
   });
