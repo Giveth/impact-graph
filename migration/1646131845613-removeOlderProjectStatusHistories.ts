43b0c5f62d3ea8230dcbd2fb7cd7125103d1230b
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2022-03-02 08:25:03 +0330

    Fix removeOlderProjectStatusHistories migration file

--- migration/1646131845613-removeOlderProjectStatusHistories.ts
+++ migration/1646131845613-removeOlderProjectStatusHistories.ts
@@ -4,15 +4,9 @@ export class removeOlderProjectStatusHistories1646131845613
   implements MigrationInterface
 {
   async up(queryRunner: QueryRunner): Promise<void> {
-    try {
-      // We added createdAt field, as we didnt deployed history feature to production
-      // I deleted all records then after that all records would have createdAt
-      await queryRunner.query(`DELETE FROM project_status_history`);
-    } catch (e) {
-      // We might dont have this table in some environments
-        // tslint:disable-next-line:no-console
-      console.log('removeOlderProjectStatusHistories1646131845613 error', e);
-    }
+    // We added createdAt field, as we didnt deployed history feature to production
+    // I deleted all records then after that all records would have createdAt
+    await queryRunner.query(`DROP TABLE IF EXISTS project_status_history`);
   }
 
   async down(queryRunner: QueryRunner): Promise<void> {}
