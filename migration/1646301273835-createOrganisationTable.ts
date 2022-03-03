fbbd1dea8b9fa4e04a977f9735c836383750c688
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2022-03-03 15:36:03 +0330

    Write migration for creating organization table
    related to #380

---
+++ migration/1646301273835-createOrganisationTable.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class createOrganisationTable1646301273835
+  implements MigrationInterface
+{
+  async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+                    CREATE TABLE IF NOT EXISTS public.organization
+            (
+                id SERIAL NOT NULL,
+                name text COLLATE pg_catalog."default" NOT NULL,
+                website text COLLATE pg_catalog."default",
+                CONSTRAINT "PK_472c1f99a32def1b0abb219cd67" PRIMARY KEY (id)
+            )
+        `);
+  }
+
+  async down(queryRunner: QueryRunner): Promise<void> {}
+}
