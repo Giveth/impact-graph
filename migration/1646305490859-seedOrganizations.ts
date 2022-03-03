1b31dbba412e9f6b8fe59cf64b7276d8783fcf3b
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2022-03-03 15:36:58 +0330

    Write migration for seed organizations
    related to #380

---
+++ migration/1646305490859-seedOrganizations.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class seedOrganizations1646305490859 implements MigrationInterface {
+  async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`INSERT INTO public.organization (name,website) VALUES 
+         ('Giveth','https://giveth.io'),
+         ('Trace','https://trace.giveth.io'),
+         ('Giving Block','https://thegivingblock.com'),
+         ('CHANGE','https://getchange.io')
+        ;`);
+  }
+
+  async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DELETE FROM organization`);
+  }
+}
