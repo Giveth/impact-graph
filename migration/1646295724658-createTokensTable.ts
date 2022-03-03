8ef18534af9de3ca79140d983184dad65d022d5b
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2022-03-03 15:35:40 +0330

    Write migration for creating token table
    related to #380

---
+++ migration/1646295724658-createTokensTable.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class createTokensTable1646295724658 implements MigrationInterface {
+  async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `      
+       CREATE TABLE IF NOT EXISTS public.token
+         (
+          id SERIAL NOT NULL,
+          name text COLLATE pg_catalog."default" NOT NULL,
+          symbol text COLLATE pg_catalog."default" NOT NULL,
+          address text COLLATE pg_catalog."default" NOT NULL,
+          "networkId" integer NOT NULL,
+          decimals integer NOT NULL,
+          CONSTRAINT "PK_82fae97f905930df5d62a702fc9" PRIMARY KEY (id)
+           )`,
+    );
+  }
+
+  async down(queryRunner: QueryRunner): Promise<void> {}
+}
