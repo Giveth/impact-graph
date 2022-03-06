947aefb1b251193896beca44a0b11a0850db5284
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2022-03-06 17:45:14 +0330

    Fill down function of changing pinata address migration

--- migration/1646573865245-changePinataGatewayOfImages.ts
+++ migration/1646573865245-changePinataGatewayOfImages.ts
@@ -36,5 +36,36 @@ export class changePinataGatewayOfImages1646573865245
     }
   }
 
-  async down(queryRunner: QueryRunner): Promise<void> {}
+  async down(queryRunner: QueryRunner): Promise<void> {
+    const projectTableExists = await queryRunner.hasTable('project');
+    if (projectTableExists) {
+      await queryRunner.query(
+        `
+        UPDATE
+        project
+        SET
+        image = REPLACE (
+          image,
+          'giveth.mypinata.cloud',
+          'gateway.pinata.cloud'
+        );
+`,
+      );
+    }
+
+    const userTableExists = await queryRunner.hasTable('user');
+    if (userTableExists) {
+      await queryRunner.query(
+        `UPDATE
+        public."user"
+        SET
+        avatar = REPLACE (
+        avatar,
+        'giveth.mypinata.cloud',
+        'gateway.pinata.cloud'
+);
+`,
+      );
+    }
+  }
 }
