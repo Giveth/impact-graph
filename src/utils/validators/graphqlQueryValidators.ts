0d6d67e560700ee0b6a45d47f55cefe4e216cbf1
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2022-03-21 19:23:49 +0330

    Replace @hapi/joi with joi

--- src/utils/validators/graphqlQueryValidators.ts
+++ src/utils/validators/graphqlQueryValidators.ts
@@ -1,4 +1,6 @@
-import Joi, { CustomHelpers, ObjectSchema, ValidationResult } from '@hapi/joi';
+import { CustomHelpers, ObjectSchema, ValidationResult } from 'joi';
+// tslint:disable-next-line:no-var-requires
+const Joi = require('joi');
 import { errorMessages } from '../errorMessages';
 
 const filterDateRegex = new RegExp('^[0-9]{8} [0-9]{2}:[0-9]{2}:[0-9]{2}$');
