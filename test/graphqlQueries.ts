c4bd5091daffb4477a37397476d9554fbcee7d0c
fatemeghasemi <ghasemiFateme7320@gmail.com>
2022-03-28 17:42:42 +0430

    Write test case for get user
    Related to #335

--- test/graphqlQueries.ts
+++ test/graphqlQueries.ts
@@ -501,6 +501,23 @@ export const userByAddress = `
     }
   }
 `;
+
+export const userById = `
+  query ($userId:  Int!) {
+    user(userId: $userId) {
+      id
+      firstName
+      lastName
+      name
+      email
+      avatar
+      walletAddress
+      url
+      location
+    }
+  }
+`;
+
 export const uploadImageToIpfsQuery = `
   mutation ($fileUpload: FileUploadInputType!) {
     upload(fileUpload: $fileUpload)
