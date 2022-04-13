5eb93762d2cb6472bc403b8f1a8ba3741f3fd885
fatemeghasemi <ghasemiFateme7320@gmail.com>
2022-04-13 22:21:56 +0430

    Define output type for statusReasonRepository functions
    Related to #470

--- src/repositories/statusReasonRepository.ts
+++ src/repositories/statusReasonRepository.ts
@@ -1,15 +1,19 @@
 import { ProjectStatusReason } from '../entities/projectStatusReason';
 
-export const findAllStatusReasons = async () => {
-  const query = await ProjectStatusReason.createQueryBuilder(
+export const findAllStatusReasons = async (): Promise<
+  ProjectStatusReason[]
+> => {
+  const query = ProjectStatusReason.createQueryBuilder(
     'project_status_reason',
   ).leftJoinAndSelect('project_status_reason.status', 'status');
 
   return query.getMany();
 };
 
-export const findStatusReasonsByStatusId = async (statusId: number) => {
-  const query = await ProjectStatusReason.createQueryBuilder(
+export const findStatusReasonsByStatusId = async (
+  statusId: number,
+): Promise<ProjectStatusReason[]> => {
+  const query = ProjectStatusReason.createQueryBuilder(
     'project_status_reason',
   ).leftJoinAndSelect('project_status_reason.status', 'status');
   query.where(`"statusId" = ${statusId}`);
