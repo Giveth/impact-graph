d469716e4733863d31d0676bcb4beb71549dcecc
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2022-05-02 17:05:27 +0430

    Add donor info to donations when third party creates donation
    related to #500

--- src/repositories/tokenRepository.ts
+++ src/repositories/tokenRepository.ts
@@ -1,6 +1,6 @@
 import { Token } from '../entities/token';
 
-export const findTokenByTokenAddres = async (
+export const findTokenByTokenAddress = async (
   tokenAddress: string,
 ): Promise<Token | undefined> => {
   return await Token.findOne(tokenAddress);
--- src/routers/apiGivRoutes.ts
+++ src/routers/apiGivRoutes.ts
@@ -7,11 +7,13 @@ import {
 import { errorMessages } from '../utils/errorMessages';
 import { Donation } from '../entities/donation';
 import { findProjectByWalletAddress } from '../repositories/projectRepository';
-import { findTokenByTokenAddres } from '../repositories/tokenRepository';
+import { findTokenByTokenAddress } from '../repositories/tokenRepository';
 import { ProjStatus } from '../entities/project';
 import { logger } from '../utils/logger';
 import { findTokenByNetworkAndSymbol } from '../utils/tokenUtils';
 import { ApiGivStandardError, handleExpressError } from './standardError';
+import { updateTotalDonationsOfProject } from '../services/donationService';
+import { findUserByWalletAddress } from '../repositories/userRepository';
 
 export const apiGivRouter = express.Router();
 apiGivRouter.post(
@@ -19,12 +21,30 @@ apiGivRouter.post(
   apiGivAuthentication,
   async (request: Request, response: Response) => {
     try {
+      // Add segment events , for reciever and sender
       const { body } = request;
-      const donation = await findDonationsByTransactionId(body.transactionId);
+      const {
+        anonymous,
+        fromWalletAddress,
+        toWalletAddress,
+        amount,
+        nonce,
+        transactionId,
+        transactionNetworkId,
+        currency,
+        tokenAddress,
+        status,
+        donationType,
+        segmentNotified,
+        isFiat,
+        priceUsd,
+        valueUsd,
+      } = body;
+      const donation = await findDonationsByTransactionId(transactionId);
       if (donation) {
         throw new ApiGivStandardError(errorMessages.DUPLICATE_TX_HASH, 400);
       }
-      const project = await findProjectByWalletAddress(body.toWalletAddress);
+      const project = await findProjectByWalletAddress(toWalletAddress);
       if (!project) {
         throw new ApiGivStandardError(errorMessages.PROJECT_NOT_FOUND, 400);
       }
@@ -34,41 +54,42 @@ apiGivRouter.post(
           400,
         );
       }
-      if (body.amount <= 0) {
+      const donor = await findUserByWalletAddress(fromWalletAddress);
+      if (amount <= 0) {
         throw new ApiGivStandardError(errorMessages.AMOUNT_IS_INVALID, 400);
       }
 
-      const token = body.tokenAddress
-        ? await findTokenByTokenAddres(body.tokenAddress)
-        : await findTokenByNetworkAndSymbol(
-            body.transactionNetworkId,
-            body.currency,
-          );
+      const token = tokenAddress
+        ? await findTokenByTokenAddress(tokenAddress)
+        : await findTokenByNetworkAndSymbol(transactionNetworkId, currency);
       if (!token) {
         throw new ApiGivStandardError(errorMessages.TOKEN_NOT_FOUND, 400);
       }
       const donationData = {
-        fromWalletAddress: body.fromWalletAddress,
-        toWalletAddress: body.toWalletAddress,
+        fromWalletAddress,
+        toWalletAddress,
+        user: donor,
+        anonymous,
         isProjectVerified: project.verified,
         project,
-        amount: body.amount,
-        valueUsd: body.valueUsd,
-        priceUsd: body.priceUsd,
-        currency: body.currency,
-        transactionId: body.transactionId,
-        nonce: body.nonce,
-        transactionNetworkId: body.transactionNetworkId,
+        amount,
+        valueUsd,
+        priceUsd,
+        currency,
+        transactionId,
+        nonce,
+        transactionNetworkId,
         createdAt: new Date(),
-        status: body.status,
-        donationType: body.donationType,
-        tokenAddress: body.tokenAddress,
-        segmentNotified: body.segmentNotified,
-        isFiat: body.isFiat,
+        status,
+        donationType,
+        tokenAddress,
+        segmentNotified,
+        isFiat,
         isTokenEligibleForGivback: token.isGivbackEligible,
         speedup: false,
       };
       const result = await Donation.create(donationData).save();
+      await updateTotalDonationsOfProject(project.id);
 
       response.send(result);
     } catch (e) {
