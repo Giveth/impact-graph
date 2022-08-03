f62962bee54140bb7f369b6b6b029b0d408ca4d3
Carlos Quintero <carlos.quintero096@gmail.com>
2022-08-02 21:41:28 -0500

    fix entry.js for adminbro

--- .adminbro/.entry.js
+++ .adminbro/.entry.js
@@ -1,17 +1,7 @@
 AdminBro.UserComponents = {}
 import Component1 from '../src/server/components/ListOrganizationsNames'
 AdminBro.UserComponents.Component1 = Component1
-import Component2 from '../src/server/components/ListOrganizationsNames'
+import Component2 from '../src/server/components/ListProjectAddresses'
 AdminBro.UserComponents.Component2 = Component2
-import Component3 from '../src/server/components/VerificationFormMilestones'
+import Component3 from '../src/server/components/FilterListedComponent'
 AdminBro.UserComponents.Component3 = Component3
-import Component4 from '../src/server/components/ListOrganizationsNames'
-AdminBro.UserComponents.Component4 = Component4
-import Component5 from '../src/server/components/ListOrganizationsNames'
-AdminBro.UserComponents.Component5 = Component5
-import Component6 from '../src/server/components/VerificationFormSocials'
-AdminBro.UserComponents.Component6 = Component6
-import Component7 from '../src/server/components/ListProjectAddresses'
-AdminBro.UserComponents.Component7 = Component7
-import Component8 from '../src/server/components/FilterListedComponent'
-AdminBro.UserComponents.Component8 = Component8
