0080c051185687770f377faf0fdc43114cf0d0b3
Amin Latifi <a.latifi.al@gmail.com>
2022-08-30 11:20:34 +0430

    Changed powerBoosting columns

--- src/entities/powerBoosting.ts
+++ src/entities/powerBoosting.ts
@@ -12,7 +12,7 @@ import {
 import { Field, ID, ObjectType } from 'type-graphql';
 import { Project } from './project';
 import { User } from './user';
-import { IsInt, Max, Min } from 'class-validator';
+import { Max, Min, IsNumber } from 'class-validator';
 
 @Entity()
 @ObjectType()
@@ -21,24 +21,30 @@ export class PowerBoosting extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Index()
   @Field(type => Project)
   @ManyToOne(type => Project, { eager: true })
   project: Project;
-  @RelationId((powerBoosting: PowerBoosting) => powerBoosting.project)
-  projectId: number;
 
   @Index()
+  @Field(type => ID)
+  @RelationId((powerBoosting: PowerBoosting) => powerBoosting.project)
+  @Column({ nullable: false })
+  projectId: number;
+
   @Field(type => User)
   @ManyToOne(type => User, { eager: true })
   user: User;
+
+  @Index()
+  @Field(type => User)
   @RelationId((powerBoosting: PowerBoosting) => powerBoosting.user)
+  @Column({ nullable: false })
   userId: number;
 
   @Field()
   @Column()
   // https://orkhan.gitbook.io/typeorm/docs/validation
-  @IsInt()
+  @IsNumber()
   @Min(0)
   @Max(100)
   percentage: number;
