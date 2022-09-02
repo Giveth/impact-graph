af77f51c9d91df1d9f2efeb0716ea632222113dc
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2022-09-02 11:18:49 +0430

    Sync user powers cronjob, not completed

---
+++ src/entities/userPower.ts
@@ -0,0 +1,44 @@
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  Index,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  RelationId,
+  Unique,
+} from 'typeorm';
+import { Field, ID, ObjectType } from 'type-graphql';
+import { User } from './user';
+
+@ObjectType()
+@Entity()
+@Unique(['userId', 'givbackRound'])
+export class UserPower extends BaseEntity {
+  @Field(type => ID)
+  @PrimaryGeneratedColumn()
+  readonly id: number;
+
+  @Index()
+  @Field(type => User, { nullable: true })
+  @ManyToOne(type => User, { eager: true, nullable: true })
+  user: User;
+  @RelationId((userPower: UserPower) => userPower.user)
+  userId: number;
+
+  @Field()
+  @Column()
+  power: number;
+
+  @Field()
+  @Column()
+  fromTimestamp: number;
+
+  @Field()
+  @Column()
+  toTimestamp: number;
+
+  @Field()
+  @Column()
+  givbackRound: number;
+}
