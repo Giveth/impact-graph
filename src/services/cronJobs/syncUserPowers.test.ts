8d06df0e7dbea57ab304cc321e781043f1af21b9
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2022-09-08 09:52:21 +0430

    Improve sync user power test cases

--- src/services/cronJobs/syncUserPowers.test.ts
+++ src/services/cronJobs/syncUserPowers.test.ts
@@ -5,16 +5,21 @@ import {
   runSyncUserPowersCronJob,
 } from './syncUserPowers';
 import {
+  createProjectData,
   generateRandomEtheriumAddress,
+  saveProjectDirectlyToDb,
   saveUserDirectlyToDb,
   sleep,
 } from '../../../test/testUtils';
 import {
   findUserPowerByUserIdAndRound,
   findUsersThatDidntSyncTheirPower,
+  insertNewUserPowers,
 } from '../../repositories/userPowerRepository';
 import { assert } from 'chai';
 import { logger } from '../../utils/logger';
+import { insertSinglePowerBoosting } from '../../repositories/powerBoostingRepository';
+import { create } from 'domain';
 
 describe(
   'runSyncUserPowersCronJob() test cases',
@@ -29,6 +34,12 @@ function runSyncUserPowersCronJobTestCases() {
     const firstUser = await saveUserDirectlyToDb(
       generateRandomEtheriumAddress(),
     );
+    const project = await saveProjectDirectlyToDb(createProjectData());
+    await insertSinglePowerBoosting({
+      user: firstUser,
+      project,
+      percentage: 20,
+    });
     const { previousGivbackRound } = getPreviousGivbackRoundInfo();
     const userPower = await findUserPowerByUserIdAndRound({
       userId: firstUser.id,
@@ -49,17 +60,29 @@ function runSyncUserPowersCronJobTestCases() {
   });
 
   it('should Fill lots of user powers after calling addSyncUserPowerJobsToQueue', async () => {
-    for (let i = 0; i < 220; i++) {
-      await saveUserDirectlyToDb(generateRandomEtheriumAddress());
+    const project = await saveProjectDirectlyToDb(createProjectData());
+    const numberOfInsertedUsers = 220;
+    for (let i = 0; i < numberOfInsertedUsers; i++) {
+      const user = await saveUserDirectlyToDb(generateRandomEtheriumAddress());
+      await insertSinglePowerBoosting({
+        user,
+        project,
+        percentage: 20,
+      });
     }
+    const { previousGivbackRound } = getPreviousGivbackRoundInfo();
+
+    const [, countBefore] = await findUsersThatDidntSyncTheirPower(
+      previousGivbackRound,
+    );
+    assert.isTrue(countBefore >= numberOfInsertedUsers);
+
     await addSyncUserPowerJobsToQueue();
 
     // make sure queue runs all the job
     await sleep(2000);
 
-    const { previousGivbackRound } = getPreviousGivbackRoundInfo();
-
-    const [users, count] = await findUsersThatDidntSyncTheirPower(
+    const [, count] = await findUsersThatDidntSyncTheirPower(
       previousGivbackRound,
     );
     assert.equal(
