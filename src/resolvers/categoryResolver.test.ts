05c2572eb07106be8b3db33691abda38c73fc86b
Carlos Quintero <carlos.quintero096@gmail.com>
2022-09-12 23:15:02 -0500

    order alphabetically both main and categories

--- src/resolvers/categoryResolver.test.ts
+++ src/resolvers/categoryResolver.test.ts
@@ -15,13 +15,38 @@ describe('mainCategoryTestCases() test cases', mainCategoryTestCases);
 describe('categoryTestCases() test cases', categoryTestCases);
 
 function mainCategoryTestCases() {
-  it('Should show main categories', async () => {
-    const title = generateRandomString(10);
-    await saveMainCategoryDirectlyToDb({
+  it('Should show main categories and categories ordered alphabetically', async () => {
+    const title = 'z' + generateRandomString(10); // ensure it's last
+    const mainCategory = await saveMainCategoryDirectlyToDb({
       banner: '',
       description: '',
       slug: generateRandomString(10),
       title,
+    });
+
+    const title2 = 'a' + generateRandomString(10); // ensure it's first
+    const mainCategory2 = await saveMainCategoryDirectlyToDb({
+      banner: '',
+      description: '',
+      slug: generateRandomString(10),
+      title: title2,
+    });
+
+    // save it's categories to sort them
+    const name = 'z' + generateRandomString(10); // ensure it's last
+    const category1 = await saveCategoryDirectlyToDb({
+      name,
+      mainCategory,
+      value: 'Agriculture',
+      isActive: true,
+    });
+
+    const name2 = 'a' + generateRandomString(10); // ensure it's first
+    const category2 = await saveCategoryDirectlyToDb({
+      name: name2,
+      mainCategory,
+      value: 'Agriculture',
+      isActive: true,
     });
 
     const categoryResponse = await axios.post(graphqlUrl, {
@@ -31,13 +56,17 @@ function mainCategoryTestCases() {
     const result = categoryResponse.data.data.mainCategories;
     assert.isNotEmpty(result);
 
-    assert.equal(result[result.length - 1].title, title);
+    assert.equal(result[0].title, mainCategory2.title);
+    assert.equal(result[result.length - 1].title, mainCategory.title);
+
+    // assert categories ordered alphabetically for maincategory1
+    assert.equal(result[result.length - 1].categories[0].name, category2.name);
+    assert.equal(result[result.length - 1].categories[1].name, category1.name);
   });
 }
 
 function categoryTestCases() {
-  it('Should show  categories', async () => {
-    const name = generateRandomString(10);
+  it('Should show categories ordered alphabetically', async () => {
     const mainCategory = await saveMainCategoryDirectlyToDb({
       banner: '',
       description: '',
@@ -45,23 +74,31 @@ function categoryTestCases() {
       title: 'Economics & Infrastructure test2',
     });
 
-    await saveCategoryDirectlyToDb({
+    const name = 'zz' + generateRandomString(10); // ensure it's last
+    const category1 = await saveCategoryDirectlyToDb({
       name,
       mainCategory,
       value: 'Agriculture',
       isActive: true,
     });
+
+    const name2 = 'aa' + generateRandomString(10); // assert it's first
+    const category2 = await saveCategoryDirectlyToDb({
+      name: name2,
+      mainCategory,
+      value: 'Agriculture',
+      isActive: true,
+    });
+
     const categoryResponse = await axios.post(graphqlUrl, {
       query: getCategoryData,
       variables: {},
     });
     const result = categoryResponse?.data?.data?.categories;
     assert.isNotEmpty(result);
-    assert.equal(
-      result[result.length - 1].mainCategory.title,
-      mainCategory.title,
-    );
+    assert.equal(result[0].mainCategory.title, mainCategory.title);
 
-    assert.equal(result[result.length - 1].name, name);
+    assert.equal(result[0].name, category2.name);
+    assert.equal(result[result.length - 1].name, category1.name);
   });
 }
