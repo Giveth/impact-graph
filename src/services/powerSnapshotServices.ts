901b4bdeafb59edfe5db548d3e7d8e47da05d226
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2022-09-29 09:38:00 +0330

    Add log to fillIncompletePowerSnapshots

--- src/services/powerSnapshotServices.ts
+++ src/services/powerSnapshotServices.ts
@@ -5,9 +5,14 @@ import {
 import { getBlockByTime } from './blockByDateService';
 import { getTimestampInSeconds } from '../utils/utils';
 import { getRoundNumberByDate } from '../utils/powerBoostingUtils';
+import { logger } from '../utils/logger';
 
 export const fillIncompletePowerSnapshots = async (): Promise<void> => {
   const incompletePowerSnapshots = await findInCompletePowerSnapShots();
+  logger.debug(
+    'fillIncompletePowerSnapshots incompletePowerSnapshots',
+    JSON.stringify(incompletePowerSnapshots, null, 2),
+  );
   for (const powerSnapshot of incompletePowerSnapshots) {
     const roundNumber = getRoundNumberByDate(powerSnapshot.time).round;
     const blockNumber = await getBlockByTime(
