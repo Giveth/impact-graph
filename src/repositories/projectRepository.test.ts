64a2b9db494d2cf9641851261fe05686672fc033
Carlos Quintero <carlos.quintero096@gmail.com>
2022-10-06 01:44:08 -0500

    fix revoke query test

--- src/repositories/projectRepository.test.ts
+++ src/repositories/projectRepository.test.ts
@@ -40,8 +40,9 @@ function projectsWithoutUpdateAfterTimeFrameTestCases() {
       title: String(new Date().getTime()),
       slug: String(new Date().getTime()),
       verified: true,
+      updatedAt: moment().subtract(1001, 'days').endOf('day').toDate(),
       projectUpdateCreationDate: moment()
-        .subtract(1000, 'days')
+        .subtract(1001, 'days')
         .endOf('day')
         .toDate(),
     });
@@ -51,6 +52,7 @@ function projectsWithoutUpdateAfterTimeFrameTestCases() {
       title: String(new Date().getTime()),
       slug: String(new Date().getTime()),
       verified: true,
+      updatedAt: moment().subtract(900, 'days').endOf('day').toDate(),
       projectUpdateCreationDate: moment()
         .subtract(900, 'days')
         .endOf('day')
