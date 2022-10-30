4cf920327744b240d53aaab32bca5f6df0b53093
Carlos Quintero <carlos.quintero096@gmail.com>
2022-10-30 13:53:16 -0500

    add additional security to report queries

--- src/resolvers/projectResolver.ts
+++ src/resolvers/projectResolver.ts
@@ -86,6 +86,7 @@ import { NETWORK_IDS } from '../provider';
 import { getVerificationFormByProjectId } from '../repositories/projectVerificationRepository';
 import {
   getDonationsQueryValidator,
+  resourcePerDateReportValidator,
   validateWithJoiSchema,
 } from '../utils/validators/graphqlQueryValidators';
 
@@ -1040,6 +1041,10 @@ export class ProjectResolver {
     @Arg('toDate', { nullable: true }) toDate?: string,
   ): Promise<Number> {
     try {
+      validateWithJoiSchema(
+        { fromDate, toDate },
+        resourcePerDateReportValidator,
+      );
       const query = this.projectRepository.createQueryBuilder('project');
 
       if (fromDate) {
