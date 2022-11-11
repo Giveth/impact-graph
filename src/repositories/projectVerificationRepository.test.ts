d7522cafa6cfe589d60785ff29de72eb4116f345
Carlos Quintero <carlos.quintero096@gmail.com>
2022-11-11 19:07:23 +0300

    add impact and update input type

--- src/repositories/projectVerificationRepository.test.ts
+++ src/repositories/projectVerificationRepository.test.ts
@@ -225,6 +225,9 @@ function updateMilestonesOfProjectVerificationTestCases() {
       achievedMilestonesProofs: ['ipfsHash'],
       foundationDate: new Date().toString(),
       mission: 'Make world a better place',
+      problem: 'Also Make the world a better place',
+      plans: 'Planning to make the world a better place',
+      impact: 'Did make the world a better place',
     };
     const updatedProjectVerification =
       await updateMilestonesOfProjectVerification({
