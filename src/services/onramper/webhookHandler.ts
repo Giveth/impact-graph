a234fa23cef516da32db80fcd882286999a06754
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2022-11-16 09:40:14 +0300

    Replace console.logs with logger

--- src/services/onramper/webhookHandler.ts
+++ src/services/onramper/webhookHandler.ts
@@ -6,13 +6,11 @@ import { logger } from '../../utils/logger';
 export async function onramperWebhookHandler(request, response) {
   try {
     // just log to test stuff
-    // tslint:disable-next-line:no-console
-    console.log(
+    logger.info(
       'onramperWebhookHandler header',
       request.headers['X-Onramper-Webhook-Signature'],
     );
-    // tslint:disable-next-line:no-console
-    console.log('onramperWebhookHandler body', JSON.stringify(request.body));
+    logger.info('onramperWebhookHandler body', JSON.stringify(request.body));
     // logic is on another branch this is temporary
     response.status(200).send('Not implemented');
   } catch (error) {
