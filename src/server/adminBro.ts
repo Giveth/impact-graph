3fe4d542b86b266cd00db1b539b59f1b808c2e57
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2023-01-04 11:14:30 +0330

    Add isFiat in donations page to admin bro

--- src/server/adminBro.ts
+++ src/server/adminBro.ts
@@ -707,7 +707,7 @@ const getAdminBroInstance = async () => {
               isVisible: false,
             },
             isFiat: {
-              isVisible: false,
+              isVisible: true,
             },
             donationType: {
               isVisible: false,
