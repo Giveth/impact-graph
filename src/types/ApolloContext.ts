ba69acc05474d654a58b9ee8d316e044c3a92d88
Amin Latifi <a.latifi.al@gmail.com>
2023-02-02 10:49:59 +0300

    Added type for Apollo Context Auth

--- src/types/ApolloContext.ts
+++ src/types/ApolloContext.ts
@@ -1,11 +1,15 @@
-import { Response } from 'express';
 import { Pool, ModuleThread } from 'threads';
 import { ProjectResolverWorker } from '../workers/projectsResolverWorker';
 import { JwtVerifiedUser } from '../services/authorizationServices';
 
+export interface AuthContextRequest {
+  token?: string;
+  error?: Error;
+}
+
 export interface ContextRequest {
   user: JwtVerifiedUser;
-  auth: any;
+  auth: AuthContextRequest;
 }
 
 export interface ApolloContext {
