41dd94e6f44d887f665159b33eebd44b703136aa
Carlos Quintero <carlos.quintero096@gmail.com>
2023-03-17 13:31:01 -0500

    add comment for matic tokens

--- src/services/donationService.test.ts
+++ src/services/donationService.test.ts
@@ -611,7 +611,7 @@ function fillOldStableCoinDonationsPriceTestCases() {
       donation,
       project,
       token,
-      ['USDC', 'MATIC'],
+      ['USDC', 'MATIC'], // For matic USDC returns more favorable values
       CHAIN_ID.POLYGON,
       amount,
     );
