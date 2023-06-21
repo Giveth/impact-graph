8201bd0ca1e27e826094db5029263af4a9f3c7b1
Koinzell <carlos.quintero096@gmail.com>
2023-06-21 17:28:36 -0500

    add optimism goerli tokens

---
+++ migration/1687383705794-AddOptimismGoerliTokens.ts
@@ -0,0 +1,57 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { Token } from '../src/entities/token';
+import seedTokens from './data/seedTokens';
+import { NETWORK_IDS } from '../src/provider';
+
+export class AddOptimismGoerliTokens1687383705794
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.manager.save(
+      Token,
+      seedTokens
+        .filter(token => token.networkId === NETWORK_IDS.OPTIMISM_GOERLI)
+        .map(t => {
+          t.address = t.address?.toLowerCase();
+          return t;
+        }),
+    );
+    const tokens = await queryRunner.query(`
+                    SELECT * FROM token
+                    WHERE "networkId" = ${NETWORK_IDS.OPTIMISM_GOERLI}
+                    `);
+    const givethOrganization = (
+      await queryRunner.query(`SELECT * FROM organization
+                  WHERE label='giveth'`)
+    )[0];
+    const traceOrganization = (
+      await queryRunner.query(`SELECT * FROM organization
+                  WHERE label='trace'`)
+    )[0];
+
+    for (const token of tokens) {
+      await queryRunner.query(`INSERT INTO organization_tokens_token ("tokenId","organizationId") VALUES
+                  (${token.id}, ${givethOrganization.id}),
+                  (${token.id}, ${traceOrganization.id})
+                ;`);
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const tokens = await queryRunner.query(`
+            SELECT * FROM token
+            WHERE "networkId" = ${NETWORK_IDS.OPTIMISM_GOERLI}
+            `);
+    await queryRunner.query(
+      `DELETE FROM organization_tokens_token WHERE "tokenId" IN (${tokens
+        .map(token => token.id)
+        .join(',')})`,
+    );
+    await queryRunner.query(
+      `
+                DELETE from token
+                WHERE "networkId" = ${NETWORK_IDS.OPTIMISM_GOERLI}
+            `,
+    );
+  }
+}
