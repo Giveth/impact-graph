1847c1df0208d33f5bf487fcd0a9dcdab53b055b
Amin Latifi <a.latifi.al@gmail.com>
2023-09-03 13:46:00 +0330

    Fixed param passing to getLatestBalances of power balance aggregator

--- src/adapters/givPowerBalanceAggregator/givPowerBalanceAggregatorAdapter.ts
+++ src/adapters/givPowerBalanceAggregator/givPowerBalanceAggregatorAdapter.ts
@@ -66,9 +66,14 @@ export class GivPowerBalanceAggregatorAdapter
   async getLatestBalances(
     params: LatestBalanceInputParams,
   ): Promise<BalanceResponse[]> {
+    const data = {
+      network: params.network,
+      networks: params.networks,
+      addresses: params.addresses.join(','),
+    };
     try {
       const response = await axios.get(`${this.baseUrl}/power-balance`, {
-        params,
+        params: data,
       });
       return response.data.map(balance => formatResponse(balance));
     } catch (e) {
