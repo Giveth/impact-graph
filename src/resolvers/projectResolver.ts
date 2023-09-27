db91549d3e67a3b1ecc924499654021e7b2f73ad
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2023-09-27 12:30:24 +0330

    Add Other types of campaigns to projectBySlug webservice
    https://github.com/Giveth/impact-graph/issues/1051#issuecomment-1733713799

--- src/resolvers/projectResolver.ts
+++ src/resolvers/projectResolver.ts
@@ -77,6 +77,7 @@ import {
   FilterProjectQueryInputParams,
   filterProjectsQuery,
   findProjectById,
+  findProjectBySlugWithoutAnyJoin,
   totalProjectsPerDate,
   totalProjectsPerDateByMonthAndYear,
   userIsOwnerOfProject,
@@ -108,6 +109,7 @@ import { FeaturedUpdate } from '../entities/featuredUpdate';
 import { PROJECT_UPDATE_CONTENT_MAX_LENGTH } from '../constants/validators';
 import { calculateGivbackFactor } from '../services/givbackService';
 import { ProjectBySlugResponse } from './types/projectResolver';
+import { getAllProjectsRelatedToActiveCampaigns } from '../services/campaignService';
 
 @ObjectType()
 class AllProjects {
@@ -898,11 +900,18 @@ export class ProjectResolver {
       isOwnerOfProject = await userIsOwnerOfProject(viewerUserId, slug);
     }
 
+    const minimalProject = await findProjectBySlugWithoutAnyJoin(slug);
+    if (!minimalProject) {
+      throw new Error(i18n.__(translationErrorMessagesKeys.PROJECT_NOT_FOUND));
+    }
+    const campaignSlugs = (await getAllProjectsRelatedToActiveCampaigns())[
+      minimalProject.id
+    ];
+
     let query = this.projectRepository
       .createQueryBuilder('project')
-      // check current slug and previous slugs
-      .where(`:slug = ANY(project."slugHistory") or project.slug = :slug`, {
-        slug,
+      .where(`project.id = :id`, {
+        id: minimalProject.id,
       })
       .leftJoinAndSelect('project.status', 'status')
       .leftJoinAndSelect(
@@ -922,9 +931,10 @@ export class ProjectResolver {
         'project.campaigns',
         Campaign,
         'campaigns',
-        '(campaigns."relatedProjectsSlugs" && ARRAY[:slug]::text[] OR campaigns."relatedProjectsSlugs" && project."slugHistory") AND campaigns."isActive" = TRUE',
+        '((campaigns."relatedProjectsSlugs" && ARRAY[:slug]::text[] OR campaigns."relatedProjectsSlugs" && project."slugHistory") AND campaigns."isActive" = TRUE) OR (campaigns.slug = ANY(:campaignSlugs))',
         {
           slug,
+          campaignSlugs,
         },
       )
       .leftJoin('project.adminUser', 'user')
@@ -946,9 +956,6 @@ export class ProjectResolver {
     });
 
     const project = await query.getOne();
-    if (!project) {
-      throw new Error(i18n.__(translationErrorMessagesKeys.PROJECT_NOT_FOUND));
-    }
     canUserVisitProject(project, String(user?.userId));
     const verificationForm =
       project?.projectVerificationForm ||
@@ -956,7 +963,9 @@ export class ProjectResolver {
     if (verificationForm) {
       (project as Project).verificationFormStatus = verificationForm?.status;
     }
-    const { givbackFactor } = await calculateGivbackFactor(project.id);
+
+    // We know that we have the project because if we reach this line means minimalProject is not null
+    const { givbackFactor } = await calculateGivbackFactor(project!.id);
 
     return { ...project, givbackFactor };
   }
