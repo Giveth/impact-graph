c7447c8c3241539d5e403c7a016c569bcc356bf0
Carlos <carlos.quintero096@gmail.com>
2023-12-21 17:22:06 -0500

    fix seed tokens

--- migration/data/seedTokens.ts
+++ migration/data/seedTokens.ts
@@ -1253,15 +1253,9 @@ const seedTokens: ITokenData[] = [
   {
     name: 'Solana native token',
     symbol: 'SOL',
-<<<<<<< Updated upstream
-    address: '0x0000000000000000000000000000000000000000',
-    decimals: 9,
-    networkId: NETWORK_IDS.SOLANA,,
-=======
     address: SOLANA_SYSTEM_ADDRESS,
-    decimals: 18,
-    networkId: 0,
->>>>>>> Stashed changes
+    decimals: 9,
+    networkId: NETWORK_IDS.SOLANA,
     chainType: ChainType.SOLANA,
     coingeckoId: COINGECKO_TOKEN_IDS.SOLANA,
   },
