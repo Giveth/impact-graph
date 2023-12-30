9d72f66fb0545f1f27d16a1b9d20ee16614a167d
Carlos <carlos.quintero096@gmail.com>
2023-12-30 01:41:01 -0500

    add test and regex for solana token addresses

--- src/utils/validators/graphqlQueryValidators.ts
+++ src/utils/validators/graphqlQueryValidators.ts
@@ -20,7 +20,7 @@ const resourcePerDateRegex = new RegExp(
 );
 
 const ethereumWalletAddressRegex = /^0x[a-fA-F0-9]{40}$/;
-const solanaWalletAddressRegex = /^[A-Za-z0-9]{43,44}$/;
+const solanaWalletAddressRegex = /^(?:[A-Za-z0-9]{32}|[A-Za-z0-9]{43,44})$/;
 const txHashRegex = /^0x[a-fA-F0-9]{64}$/;
 const solanaTxRegex = /^[A-Za-z0-9]{88}$/; // TODO: Is this enough? We are using the signature to fetch transactions
 const tokenSymbolRegex = /^[a-zA-Z0-9]{2,10}$/; // OPTIMISTIC OP token is 2 chars long
@@ -95,7 +95,11 @@ export const createDonationQueryValidator = Joi.object({
   transactionNetworkId: Joi.string()
     .required()
     .valid(...Object.values(NETWORK_IDS)),
-  tokenAddress: Joi.string().pattern(ethereumWalletAddressRegex),
+  tokenAddress: Joi.when('transactionNetworkId', {
+    is: 0, // if its solana network
+    then: Joi.string().pattern(solanaWalletAddressRegex),
+    otherwise: Joi.string().pattern(ethereumWalletAddressRegex),
+  }),
   token: Joi.string()
     .required()
     .pattern(tokenSymbolRegex)
