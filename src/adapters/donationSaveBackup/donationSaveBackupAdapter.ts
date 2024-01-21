cad6c78ecb02ea2da42cc7ebb8346b28967288a3
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2024-01-21 13:21:14 +0330

    Change loading urls for backup service

--- src/adapters/donationSaveBackup/donationSaveBackupAdapter.ts
+++ src/adapters/donationSaveBackup/donationSaveBackupAdapter.ts
@@ -12,6 +12,13 @@ const DONATION_SAVE_BACKUP_API_URL = config.get(
 const DONATION_SAVE_BACKUP_API_SECRET = config.get(
   'DONATION_SAVE_BACKUP_API_SECRET',
 ) as string;
+const DONATION_SAVE_BACKUP_DATA_SOURCE = config.get(
+  'DONATION_SAVE_BACKUP_DATA_SOURCE',
+) as string;
+const DONATION_SAVE_BACKUP_COLLECTION =
+  config.get('DONATION_SAVE_BACKUP_COLLECTION') || 'failed_donation';
+const DONATION_SAVE_BACKUP_DATABASE =
+  config.get('DONATION_SAVE_BACKUP_DATABASE') || 'failed_donation';
 
 // add '/' if doesn't exist at the
 const baseUrl = DONATION_SAVE_BACKUP_API_URL.endsWith('/')
@@ -23,11 +30,11 @@ export const getNotImportedDonationsFromBackup = async (
   skip = 0,
 ) => {
   const result = await axios.post(
-    new URL('find', baseUrl).href,
+    `${baseUrl}find`,
     {
-      collection: 'failed_donation',
-      database: 'failed_donation',
-      dataSource: 'staging-giveth-donation-backupservice',
+      collection: DONATION_SAVE_BACKUP_COLLECTION,
+      database: DONATION_SAVE_BACKUP_DATABASE,
+      dataSource: DONATION_SAVE_BACKUP_DATA_SOURCE,
       limit,
       skip,
       filter: {
@@ -53,11 +60,11 @@ export const getNotImportedDonationsFromBackup = async (
 
 export const getSingleDonationFromBackupByTxHash = async (txHash: string) => {
   const result = await axios.post(
-    new URL('findOne', baseUrl).href,
+    `${baseUrl}findOne`,
     {
-      collection: 'failed_donation',
-      database: 'failed_donation',
-      dataSource: 'staging-giveth-donation-backupservice',
+      collection: DONATION_SAVE_BACKUP_COLLECTION,
+      database: DONATION_SAVE_BACKUP_DATABASE,
+      dataSource: DONATION_SAVE_BACKUP_DATA_SOURCE,
       filter: {
         txHash,
       },
@@ -79,11 +86,11 @@ export const getSingleDonationFromBackupByTxHash = async (txHash: string) => {
 };
 export const getSingleDonationFromBackupById = async (id: string) => {
   const result = await axios.post(
-    new URL('findOne', baseUrl).href,
+    `${baseUrl}findOne`,
     {
-      collection: 'failed_donation',
-      database: 'failed_donation',
-      dataSource: 'staging-giveth-donation-backupservice',
+      collection: DONATION_SAVE_BACKUP_COLLECTION,
+      database: DONATION_SAVE_BACKUP_DATABASE,
+      dataSource: DONATION_SAVE_BACKUP_DATA_SOURCE,
       filter: {
         _id: { $oid: id },
       },
@@ -106,11 +113,11 @@ export const getSingleDonationFromBackupById = async (id: string) => {
 
 export const markDonationAsImported = async (id: string) => {
   const result = await axios.post(
-    new URL('updateOne', baseUrl).href,
+    `${baseUrl}updateOne`,
     {
-      collection: 'failed_donation',
-      database: 'failed_donation',
-      dataSource: 'staging-giveth-donation-backupservice',
+      collection: DONATION_SAVE_BACKUP_COLLECTION,
+      database: DONATION_SAVE_BACKUP_DATABASE,
+      dataSource: DONATION_SAVE_BACKUP_DATA_SOURCE,
       filter: {
         _id: { $oid: id },
       },
@@ -138,11 +145,11 @@ export const markDonationAsImported = async (id: string) => {
 
 export const unmarkDonationAsImported = async (id: string) => {
   const result = await axios.post(
-    new URL('updateOne', baseUrl).href,
+    `${baseUrl}updateOne`,
     {
-      collection: 'failed_donation',
-      database: 'failed_donation',
-      dataSource: 'staging-giveth-donation-backupservice',
+      collection: DONATION_SAVE_BACKUP_COLLECTION,
+      database: DONATION_SAVE_BACKUP_DATABASE,
+      dataSource: DONATION_SAVE_BACKUP_DATA_SOURCE,
       filter: {
         _id: { $oid: id },
       },
