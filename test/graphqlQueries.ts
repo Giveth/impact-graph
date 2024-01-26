8416b0039d64a5fc886c9025cccffa7c75b3291b
Carlos <carlos.quintero096@gmail.com>
2024-01-26 17:49:54 -0500

    add qfround maximum cap to entity and logic

--- test/graphqlQueries.ts
+++ test/graphqlQueries.ts
@@ -735,6 +735,7 @@ export const fetchMultiFilterAllProjectsQuery = `
           name
           isActive
           id
+          maximumReward
         }
         totalReactions
         totalDonations
