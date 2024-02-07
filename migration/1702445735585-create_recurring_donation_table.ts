5d8aaa7ea1644eadc202e2c10395d49e0c2aca98
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2024-02-07 12:08:49 +0330

    Fix create recurring donation migration

--- migration/1702445735585-create_recurring_donation_table.ts
+++ migration/1702445735585-create_recurring_donation_table.ts
@@ -8,23 +8,23 @@ export class createRecurringDonationTable1702445735585
                 CREATE TABLE recurring_donation (
                 id SERIAL PRIMARY KEY,
                 network_id INT NOT NULL,
-                tx_hash text NOT NULL,
-                project_id INT,
-                anchor_contract_address_id INT,
-                donor_id INT,
-                updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-                created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+                "txHash" text NOT NULL,
+                "projectId" INT,
+                "anchorContractAddressId" INT,
+                "donorId" INT,
+                "updatedAt" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+                "createdAt" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
                 finished BOOLEAN, 
                 CONSTRAINT fk_project
-                    FOREIGN KEY(project_id) 
+                    FOREIGN KEY("projectId") 
                     REFERENCES project(id),
                 CONSTRAINT fk_anchor_contract_address
-                    FOREIGN KEY(anchor_contract_address_id) 
+                    FOREIGN KEY("anchorContractAddressId") 
                     REFERENCES anchor_contract_address(id),
                 CONSTRAINT fk_donor
                     FOREIGN KEY(donor_id) 
                     REFERENCES "user"(id),
-                UNIQUE(tx_hash, network_id, project_id)
+                UNIQUE("txHash", "networkId", "projectId")
             );
             
             CREATE INDEX idx_tx_hash ON recurring_donation(tx_hash);
