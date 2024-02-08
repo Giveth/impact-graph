c5052d8cb25c49e9802698bfbca82236301aa928
Ramin <raminramazanpour@gmail.com>
2024-02-08 13:29:45 +0100

    Migrate from Autopilot to Ortto (#1279)
    * add transactionLink and projectLink
    
    * add blockExplorer URLs
    
    * add blockExplorer URLs
    
    * fetch token instead from token address
    
    * add "endOfLine": "auto" to eslint
    
    * replace segment with emailData
    
    * rename getSegmentProjectAttributes and getEmailDataAttributes
    
    * remove analyticsUserId and anonymousId
    
    * replace emailData with segment
    
    * add findUserById to getEmailDataProjectAttributes
    
    * disable sending email for projectSavedAsDraft and projectDeactivated
    
    * activate projectPublished notification

---
+++ src/utils/networksConfig.ts
@@ -0,0 +1,40 @@
+const networksConfig = {
+  '1': {
+    blockExplorer: 'https://etherscan.io/',
+  },
+  '5': {
+    blockExplorer: 'https://goerli.etherscan.io/',
+  },
+  '10': {
+    blockExplorer: 'https://optimistic.etherscan.io/',
+  },
+  '61': {
+    blockExplorer: 'https://etc.blockscout.com/',
+  },
+  '63': {
+    blockExplorer: 'https://etc-mordor.blockscout.com/',
+  },
+  '100': {
+    blockExplorer: 'https://gnosis.blockscout.com/',
+  },
+  '101': {
+    blockExplorer: 'https://explorer.solana.com/',
+  },
+  '103': {
+    blockExplorer: 'https://explorer.solana.com/',
+  },
+  '137': {
+    blockExplorer: 'https://polygonscan.com/',
+  },
+  '420': {
+    blockExplorer: 'https://goerli-optimism.etherscan.io/',
+  },
+  '42220': {
+    blockExplorer: 'https://explorer.celo.org/mainnet/',
+  },
+  '44787': {
+    blockExplorer: 'https://explorer.celo.org/alfajores/',
+  },
+};
+
+export default networksConfig;
