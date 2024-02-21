d5747b51bb5c57160ac30b678a279dee6035a15e
Amin Latifi <a.latifi.al@gmail.com>
2024-02-21 14:15:43 +0330

    Fixed pinata image upload issue
    https://github.com/Giveth/giveth-dapps-v2/issues/3779

--- src/middleware/pinataUtils.ts
+++ src/middleware/pinataUtils.ts
@@ -2,36 +2,30 @@
  * Pinata cloud functions helper
  */
 
-import Axios, { AxiosResponse } from 'axios';
-// tslint:disable-next-line:no-var-requires
-const FormData = require('form-data');
-import fs = require('fs');
+import pinataSDK, { PinataPinResponse } from '@pinata/sdk';
 
 import ReadableStream = NodeJS.ReadableStream;
 import config from '../config';
+
+let _pinata: pinataSDK;
+
+export const getPinata = (): pinataSDK => {
+  if (!_pinata) {
+    _pinata = new pinataSDK({
+      pinataApiKey: config.get('PINATA_API_KEY') as string,
+      pinataSecretApiKey: config.get('PINATA_SECRET_API_KEY') as string,
+    });
+  }
+  return _pinata;
+};
 
 export const pinFile = (
   file: ReadableStream,
   filename: String = 'untitled',
   encoding: string,
-): Promise<AxiosResponse> => {
-  const data = new FormData();
-  data.append('file', file, { filename, encoding });
-
-  if (filename) {
-    const metadata = JSON.stringify({
-      name: filename,
-    });
-    data.append('pinataMetadata', metadata);
-  }
-
-  return Axios.post('https://api.pinata.cloud/pinning/pinFileToIPFS', data, {
-    maxContentLength: Infinity, // this is needed to prevent Axios from throw error with large files
-    headers: {
-      'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
-      pinata_api_key: config.get('PINATA_API_KEY') as string,
-      pinata_secret_api_key: config.get('PINATA_SECRET_API_KEY') as string,
-    },
+): Promise<PinataPinResponse> => {
+  return getPinata().pinFileToIPFS(file, {
+    pinataMetadata: { name: filename.toString() },
   });
 };
 
@@ -39,28 +33,13 @@ export const pinFileDataBase64 = (
   fileDataBase64: string,
   filename: string = 'untitled',
   encoding: string,
-): Promise<AxiosResponse> => {
-  const data = new FormData();
+): Promise<PinataPinResponse> => {
   const array = fileDataBase64.split(',');
-
   const base64FileData =
     array.length > 1 && array[0].indexOf('base64') >= 0 ? array[1] : array[0];
   const fileData = Buffer.from(base64FileData, 'base64');
-  data.append('file', fileData, { filename, encoding });
 
-  if (filename) {
-    const metadata = JSON.stringify({
-      name: filename,
-    });
-    data.append('pinataMetadata', metadata);
-  }
-
-  return Axios.post('https://api.pinata.cloud/pinning/pinFileToIPFS', data, {
-    maxContentLength: Infinity, // this is needed to prevent Axios from throw error with large files
-    headers: {
-      'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
-      pinata_api_key: config.get('PINATA_API_KEY') as string,
-      pinata_secret_api_key: config.get('PINATA_SECRET_API_KEY') as string,
-    },
+  return getPinata().pinFileToIPFS(fileData, {
+    pinataMetadata: { name: filename.toString() },
   });
 };
