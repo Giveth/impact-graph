910a21d49a19d606de0b75217d693dd467bc0488
Ramin <raminramazanpour@gmail.com>
2024-03-14 14:15:26 +0330

    revert stripe.ts to original state

--- src/utils/stripe.ts
+++ src/utils/stripe.ts
@@ -2,11 +2,80 @@ import Stripe from 'stripe';
 
 import config from '../config';
 import { StripeTransaction } from '../entities/bankAccount';
+import { Project } from '../entities/project';
 import { logger } from './logger';
 
 const stripe = new Stripe(config.get('STRIPE_SECRET').toString(), {
   apiVersion: '2020-08-27',
 });
+
+interface CreateStripeCheckoutSessionOptions {
+  amount: number;
+  successUrl: string;
+  cancelUrl: string;
+  applicationFee?: number;
+}
+
+export async function getStripeAccountId(project: Project) {
+  if (project.stripeAccountId) return project.stripeAccountId;
+
+  const customer = await createStripeAccount(project);
+
+  return customer.id;
+}
+
+export async function createStripeAccount(project: Project) {
+  const account = await (stripe as any).accounts.create({ type: 'standard' });
+
+  project.stripeAccountId = account.id;
+
+  await Project.save(project);
+
+  return account;
+}
+
+export function createStripeAccountLink(accountId: string, refreshUrl: string) {
+  return stripe.accountLinks.create({
+    account: accountId,
+    type: 'account_onboarding',
+    refresh_url: refreshUrl,
+    return_url: refreshUrl,
+  });
+}
+
+export async function createStripeCheckoutSession(
+  project: Project,
+  options: CreateStripeCheckoutSessionOptions,
+) {
+  const fee = options.applicationFee
+    ? {
+        payment_intent_data: { application_fee_amount: options.applicationFee },
+      }
+    : {};
+  return await stripe.checkout.sessions.create(
+    {
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          name: `${project.title} donation: ($${options.amount / 100})`,
+          amount: options.amount,
+          currency: 'USD',
+          quantity: 1,
+        },
+      ],
+      success_url: options.successUrl,
+      cancel_url: options.cancelUrl,
+      ...fee,
+    },
+    {
+      stripeAccount: project.stripeAccountId || undefined,
+    },
+  );
+}
+
+export async function getStripeCheckoutSession(sessionId: string) {
+  return await stripe.checkout.sessions.retrieve(sessionId);
+}
 
 export async function handleStripeWebhook(rq, rs) {
   const sig = rq.headers['stripe-signature'];
