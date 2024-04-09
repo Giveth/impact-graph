cb8152760c58b25bcf9127d9713fafe52cf3bbc0
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2024-04-09 13:27:41 +0330

    WIP Write the document template and two sample test cases
    1459

---
+++ src/server/adminJs/adminJsPermissions.test.ts
@@ -0,0 +1,29 @@
+import { canAccessUserAction } from './adminJsPermissions';
+import { assert } from 'chai';
+import { UserRole } from '../../entities/user';
+
+describe('canAccessUserAction test cases', canAccessUserActionTestCases);
+
+function canAccessUserActionTestCases() {
+  // admin
+  it('should return true for admin --> new', function() {
+    assert.isTrue(canAccessUserAction({
+      currentAdmin: {role: UserRole.ADMIN}
+    }, 'new'));
+  });
+
+
+
+  // campaign manager
+  it('should return false for campaignManager --> new', function() {
+    assert.isFalse(canAccessUserAction({
+      currentAdmin: {role: UserRole.CAMPAIGN_MANAGER}
+    }, 'new'));
+  });
+
+
+  // reviewer
+
+
+  // operator
+}
