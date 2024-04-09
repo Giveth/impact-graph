489a9fd464ec1cdcddf37fc47ff7c4d6d9dbd710
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2024-04-09 13:41:03 +0330

    Fix eslint errors

--- src/server/adminJs/adminJsPermissions.test.ts
+++ src/server/adminJs/adminJsPermissions.test.ts
@@ -6,24 +6,30 @@ describe('canAccessUserAction test cases', canAccessUserActionTestCases);
 
 function canAccessUserActionTestCases() {
   // admin
-  it('should return true for admin --> new', function() {
-    assert.isTrue(canAccessUserAction({
-      currentAdmin: {role: UserRole.ADMIN}
-    }, 'new'));
+  it('should return true for admin --> new', function () {
+    assert.isTrue(
+      canAccessUserAction(
+        {
+          currentAdmin: { role: UserRole.ADMIN },
+        },
+        'new',
+      ),
+    );
   });
-
-
 
   // campaign manager
-  it('should return false for campaignManager --> new', function() {
-    assert.isFalse(canAccessUserAction({
-      currentAdmin: {role: UserRole.CAMPAIGN_MANAGER}
-    }, 'new'));
+  it('should return false for campaignManager --> new', function () {
+    assert.isFalse(
+      canAccessUserAction(
+        {
+          currentAdmin: { role: UserRole.CAMPAIGN_MANAGER },
+        },
+        'new',
+      ),
+    );
   });
 
-
   // reviewer
-
 
   // operator
 }
