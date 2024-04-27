37dfc5d74e8c8e657dca28ce0c0e265fbb98848f
Ramin <raminramazanpour@gmail.com>
2024-04-27 18:18:54 +0330

    add findArchivedQfRounds endpoint

--- src/resolvers/qfRoundResolver.ts
+++ src/resolvers/qfRoundResolver.ts
@@ -1,14 +1,27 @@
-import { Arg, Field, ObjectType, Query, Resolver } from 'type-graphql';
-
+import {
+  Arg,
+  Args,
+  ArgsType,
+  Field,
+  Int,
+  ObjectType,
+  Query,
+  Resolver,
+} from 'type-graphql';
+import { Service } from 'typedi';
+import { Max, Min } from 'class-validator';
 import { User } from '../entities/user';
 import {
   findActiveQfRound,
   findAllQfRounds,
+  findArchivedQfRounds,
   findQfRoundBySlug,
   getProjectDonationsSqrtRootSum,
   getQfRoundTotalProjectsDonationsSum,
 } from '../repositories/qfRoundRepository';
 import { QfRound } from '../entities/qfRound';
+import { OrderBy, OrderDirection } from './projectResolver';
+import { OrderField } from '../entities/project';
 
 @ObjectType()
 export class QfRoundStatsResponse {
@@ -34,11 +47,40 @@ export class ExpectedMatchingResponse {
   matchingPool: number;
 }
 
+@Service()
+@ArgsType()
+class QfArchivedRoundsArgs {
+  @Field(_type => Int, { defaultValue: 0 })
+  @Min(0)
+  skip: number;
+
+  @Field(_type => Int, { defaultValue: 10 })
+  @Min(0)
+  @Max(50)
+  limit: number;
+
+  @Field(_type => OrderBy, {
+    defaultValue: {
+      field: OrderField.GIVPower,
+      direction: OrderDirection.DESC,
+    },
+  })
+  orderBy: OrderBy;
+}
+
 @Resolver(_of => User)
 export class QfRoundResolver {
   @Query(_returns => [QfRound], { nullable: true })
   async qfRounds() {
     return findAllQfRounds();
+  }
+
+  @Query(() => [QfRound], { nullable: true })
+  async qfArchivedRounds(
+    @Args()
+    { limit, skip, orderBy }: QfArchivedRoundsArgs,
+  ): Promise<QfRound[] | null> {
+    return findArchivedQfRounds(limit, skip, orderBy);
   }
 
   // This will be the formula data separated by parts so frontend
