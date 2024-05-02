0b1ced24b527e99c7a22e5df017966582b6e1c09
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2024-05-02 15:19:30 +0330

    Put executing change connection idle timeout of db in try catch

--- migration/1714568966885-changeConnectionIdleTimeout.ts
+++ migration/1714568966885-changeConnectionIdleTimeout.ts
@@ -4,9 +4,13 @@ export class ChangeConnectionIdleTimeout1714568966885
   implements MigrationInterface
 {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER DATABASE "${process.env.TYPEORM_DATABASE_NAME}" SET idle_in_transaction_session_timeout = '10min';`,
-    );
+    try {
+      await queryRunner.query(
+        `ALTER DATABASE "${process.env.TYPEORM_DATABASE_NAME}" SET idle_in_transaction_session_timeout = '10min';`,
+      );
+    } catch (e) {
+      console.error('Error setting idle_in_transaction_session_timeout, try to run the query manually ...', e);
+    }
   }
 
   public async down(_queryRunner: QueryRunner): Promise<void> {
