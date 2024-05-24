b48c030db08bdb9f16dee0b886f04be37891eb24
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2024-05-24 11:00:40 +0330

    Add test cases about filteriing base network

--- src/resolvers/donationResolver.test.ts
+++ src/resolvers/donationResolver.test.ts
@@ -2611,7 +2611,7 @@ function createDonationTestCases() {
     );
     assert.equal(
       saveDonationResponse.data.errors[0].message,
-      '"transactionNetworkId" must be one of [1, 3, 5, 100, 137, 10, 11155420, 56, 42220, 44787, 61, 63, 42161, 421614, 8453, 84532,  101, 102, 103]',
+      '"transactionNetworkId" must be one of [1, 3, 5, 100, 137, 10, 11155420, 56, 42220, 44787, 61, 63, 42161, 421614, 8453, 84532, 101, 102, 103]',
     );
   });
   it('should not throw exception when currency is not valid when currency is USDC.e', async () => {
