7fb517453d8bbec60d9ad255e6876aa6e0495630
Meriem-BM <barhoumi.meriem1@gmail.com>
2024-06-27 13:02:47 +0100

    fix: findArchivedQfRounds query

--- src/repositories/qfRoundRepository.ts
+++ src/repositories/qfRoundRepository.ts
@@ -10,6 +10,7 @@ import {
 import config from '../config';
 import { ProjectEstimatedMatchingView } from '../entities/ProjectEstimatedMatchingView';
 import { Sybil } from '../entities/sybil';
+import { ProjectFraud } from '../entities/projectFraud';
 
 const qfRoundsAndMainCategoryCacheDuration =
   (config.get('QF_ROUND_AND_MAIN_CATEGORIES_CACHE_DURATION') as number) ||
@@ -141,11 +142,21 @@ export const findArchivedQfRounds = async (
           .select('COUNT(DISTINCT donation.fromWalletAddress)', 'uniqueDonors')
           .from(Donation, 'donation')
           .leftJoin(User, 'user', 'user.id = donation.userId')
-          .leftJoin(Sybil, 'sybil', 'sybil.userId = user.id')
+          .leftJoin(
+            Sybil,
+            'sybil',
+            'sybil.userId = user.id AND sybil.qfRoundId = qfRound.id',
+          )
+          .leftJoin(
+            ProjectFraud,
+            'projectFraud',
+            'projectFraud.projectId = donation.projectId AND projectFraud.qfRoundId = qfRound.id',
+          )
           .where('donation.qfRoundId = qfRound.id')
           .andWhere('donation.status = :status', { status: 'verified' })
           .andWhere('user.passportScore >= qfRound.minimumPassportScore')
           .andWhere('sybil.id IS NULL')
+          .andWhere('projectFraud.id IS NULL')
           .andWhere(
             'donation.createdAt BETWEEN qfRound.beginDate AND qfRound.endDate',
           ),
