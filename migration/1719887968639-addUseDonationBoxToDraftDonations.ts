db4e8fa64d74f2ac4ea370f89b6607c4dea849d0
ali ebrahimi <aliebrahimi2079@gmail.com>
2024-07-03 04:20:32 +0330

    fill useDonationBox field with correct data in migrations

--- migration/1719887968639-addUseDonationBoxToDraftDonations.ts
+++ migration/1719887968639-addUseDonationBoxToDraftDonations.ts
@@ -13,6 +13,30 @@ export class AddUseDonationBoxToDraftDonations1719887968639
         default: false,
       }),
     );
+
+    const givethProjectId = 1;
+    const timeDiff = 60 * 1000; // 1 minute in milliseconds
+
+    await queryRunner.query(
+      `
+      WITH RelevantDonations AS (
+        SELECT d1.id AS donation1_id, d2.id AS donation2_id
+        FROM draft_donation d1
+        JOIN draft_donation d2 ON d1."userId" = d2."userId"
+        WHERE d1."projectId" = $1
+          AND d2."projectId" != $1
+          AND ABS(EXTRACT(EPOCH FROM (d1."createdAt" - d2."createdAt"))) <= $2
+      )
+      UPDATE draft_donation
+      SET "useDonationBox" = true
+      WHERE id IN (
+        SELECT donation1_id FROM RelevantDonations
+        UNION
+        SELECT donation2_id FROM RelevantDonations
+      );
+    `,
+      [givethProjectId, timeDiff],
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
