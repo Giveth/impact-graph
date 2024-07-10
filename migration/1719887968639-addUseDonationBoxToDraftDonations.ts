dea4ebc4d57eb9e1ca218fe2543d2eba256b670e
Ramin <raminramazanpour@gmail.com>
2024-07-10 15:25:22 +0330

    fix migration error

--- migration/1719887968639-addUseDonationBoxToDraftDonations.ts
+++ migration/1719887968639-addUseDonationBoxToDraftDonations.ts
@@ -4,24 +4,34 @@ export class AddUseDonationBoxToDraftDonations1719887968639
   implements MigrationInterface
 {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.addColumn(
-      'draft_donation',
-      new TableColumn({
-        name: 'useDonationBox',
-        type: 'boolean',
-        isNullable: true,
-        default: false,
-      }),
+    const table = await queryRunner.getTable('draft_donation');
+    const useDonationBoxColumn = table?.findColumnByName('useDonationBox');
+    const relevantDonationTxHashColumn = table?.findColumnByName(
+      'relevantDonationTxHash',
     );
 
-    await queryRunner.addColumn(
-      'draft_donation',
-      new TableColumn({
-        name: 'relevantDonationTxHash',
-        type: 'varchar',
-        isNullable: true,
-      }),
-    );
+    if (!useDonationBoxColumn) {
+      await queryRunner.addColumn(
+        'draft_donation',
+        new TableColumn({
+          name: 'useDonationBox',
+          type: 'boolean',
+          isNullable: true,
+          default: false,
+        }),
+      );
+    }
+
+    if (!relevantDonationTxHashColumn) {
+      await queryRunner.addColumn(
+        'draft_donation',
+        new TableColumn({
+          name: 'relevantDonationTxHash',
+          type: 'varchar',
+          isNullable: true,
+        }),
+      );
+    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
