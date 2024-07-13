76a10fd1a3902da4c1c2fafe67f2d3cd8b2a4fee
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2024-07-13 09:22:59 +0330

    Relate all base, optimism and mainnet tokens to endaoment organization
    related to #1626

--- migration/1719740230650-add_endaoment_organization.ts
+++ migration/1719740230650-add_endaoment_organization.ts
@@ -1,4 +1,6 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
+import { ORGANIZATION_LABELS } from '../src/entities/organization';
+import { NETWORK_IDS } from '../src/provider';
 
 export class AddEndaomentOrganization1719740230650
   implements MigrationInterface
@@ -6,8 +8,25 @@ export class AddEndaomentOrganization1719740230650
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
             INSERT INTO "organization" ("name", "disableNotifications", "disableRecurringDonations", "disableUpdateEnforcement", "label", "website", "supportCustomTokens")
-            VALUES ('Endaoment', true, true, true, 'endaoment', 'https://endaoment.org', false);
+            VALUES ('Endaoment', true, true, true, '${ORGANIZATION_LABELS.ENDAOMENT}', 'https://endaoment.org', false);
         `);
+    const endaomentOrganization = (
+      await queryRunner.query(`SELECT * FROM organization
+        WHERE label='${ORGANIZATION_LABELS.ENDAOMENT}'`)
+    )[0];
+
+    const tokens = await queryRunner.query(`
+            SELECT * FROM token
+            WHERE "networkId" = ${NETWORK_IDS.BASE_MAINNET} OR "networkId" = ${NETWORK_IDS.MAIN_NET} OR "networkId" = ${NETWORK_IDS.OPTIMISTIC};
+          `);
+
+    for (const token of tokens) {
+      // Add all Base tokens to Giveth organization
+      await queryRunner.query(`  INSERT INTO organization_tokens_token ("tokenId","organizationId") VALUES
+        (${token.id}, ${endaomentOrganization!.id}),
+        ON CONFLICT DO NOTHING;
+      ;`);
+    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
