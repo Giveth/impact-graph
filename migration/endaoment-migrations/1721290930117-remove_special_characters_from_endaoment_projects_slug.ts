d90af0c26a4c624a53262f5fd463dcd5563ad896
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2024-07-20 07:51:59 +0330

    Comment migrations related to endaoment

--- migration/endaoment-migrations/1721290930117-remove_special_characters_from_endaoment_projects_slug.ts
+++ migration/endaoment-migrations/1721290930117-remove_special_characters_from_endaoment_projects_slug.ts
@@ -1,5 +1,5 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
-import { ORGANIZATION_LABELS } from '../src/entities/organization';
+import { ORGANIZATION_LABELS } from '../../src/entities/organization';
 
 export class RemoveSpecialCharactersFromEndaomentProjectsSlug1721290930117
   implements MigrationInterface
