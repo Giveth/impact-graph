64536e5abef7cc859d0ddb00a0dc4fbab6a537ce
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2024-08-20 11:14:51 +0330

    Fix update base address of endaoment projects migration

--- migration/1724067829181-update_base_address_of_endaoment_projects.ts
+++ migration/1724067829181-update_base_address_of_endaoment_projects.ts
@@ -12,14 +12,21 @@ export class UpdateBaseAddressOfEndaomentProjects1724067829181
     const endaomentOrgId = endaomentOrgIdResult[0].id;
 
     await queryRunner.query(`
-            UPDATE project_address pa_8453
-            JOIN project p ON pa_8453.projectId = p.id
-            JOIN project_address pa_1 ON pa_8453.projectId = pa_1.projectId AND pa_1.networkId =  ${NETWORK_IDS.MAIN_NET}
-            SET pa_8453.address = pa_1.address
-            WHERE pa_8453.networkId = ${NETWORK_IDS.BASE_MAINNET}
-            AND p.organizationId = ${endaomentOrgId};
+            UPDATE project_address
+            SET address = subquery.address
+            FROM (
+                SELECT pa_8453.id, pa_1.address
+                FROM project_address pa_8453
+                JOIN project p ON pa_8453."projectId" = p.id
+                JOIN project_address pa_1 ON pa_8453."projectId" = pa_1."projectId" AND pa_1."networkId" = ${NETWORK_IDS.MAIN_NET}
+                WHERE pa_8453."networkId" = ${NETWORK_IDS.BASE_MAINNET}
+                AND p."organizationId" = ${endaomentOrgId}
+            ) AS subquery
+            WHERE project_address.id = subquery.id;
         `);
   }
 
-  public async down(_queryRunner: QueryRunner): Promise<void> {}
+  public async down(_queryRunner: QueryRunner): Promise<void> {
+    // This is intentionally left empty as there is no specific rollback logic provided.
+  }
 }
