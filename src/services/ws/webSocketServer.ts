7f3286bec50f9ff47311e36b9f120e39229837d2
Meriem-BM <barhoumi.meriem1@gmail.com>
2024-08-22 09:56:52 +0100

    fix: reduce timer to 15 mins & update renew draft donation expiration date

---
+++ src/services/ws/webSocketServer.ts
@@ -0,0 +1,51 @@
+import WebSocket from 'ws';
+
+export function startWebSocketServer(server) {
+  const wss = new WebSocket.Server({ server });
+
+  // Handle WebSocket connections
+  wss.on('connection', ws => {
+    console.log('New client connected');
+
+    ws.on('message', message => {
+      console.log('Received:', message);
+    });
+
+    ws.on('close', () => {
+      console.log('Client disconnected');
+    });
+  });
+
+  // Broadcast a message to all connected clients
+  function broadcastMessage(message) {
+    wss.clients.forEach(client => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  }
+
+  // Exported function to be called from cron job
+  function notifyDonationAdded(donation) {
+    const message = JSON.stringify({
+      type: 'new-donation',
+      data: donation,
+    });
+    broadcastMessage(message);
+  }
+
+  // Exported function to to be called when marking draft donation as failed
+  function notifyDraftDonationFailed(donation) {
+    const message = JSON.stringify({
+      type: 'draft-donation-failed',
+      data: donation,
+    });
+    broadcastMessage(message);
+  }
+
+  // Export the notifyDonationAdded function so it can be used externally
+  return {
+    notifyDonationAdded,
+    notifyDraftDonationFailed,
+  };
+}
