a199e2ce3a2e3bbcc70fef9a2d33db3980162a7e
Meriem-BM <barhoumi.meriem1@gmail.com>
2024-08-26 14:20:47 +0100

    fix: add Transfer-Encoding header

--- src/services/sse/sse.ts
+++ src/services/sse/sse.ts
@@ -23,6 +23,8 @@ export function addClient(res: Response) {
   res.setHeader('Content-Type', 'text/event-stream');
   res.setHeader('Cache-Control', 'no-cache');
   res.setHeader('Connection', 'keep-alive');
+  res.setHeader('X-Accel-Buffering', 'no');
+  res.setHeader('Transfer-Encoding', 'chunked');
 
   res.flushHeaders();
 
