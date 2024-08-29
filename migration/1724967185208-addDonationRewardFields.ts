de1c44ca1e8c3a1349079f817833709af69995d6
ali ebrahimi <aliebrahimi2079@gmail.com>
2024-08-30 01:18:17 +0330

    Add autogenerated migration for adding donation reward fields

---
+++ migration/1724967185208-addDonationRewardFields.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddDonationRewardFields1724967185208
+  implements MigrationInterface
+{
+  name = 'AddDonationRewardFields1724967185208';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "donation" ADD "rewardTokenAmount" double precision`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "donation" ADD "lockedRewardTokenAmount" double precision`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "project" ADD "rewardStreamStart" TIMESTAMP`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "project" ADD "rewardStreamEnd" TIMESTAMP`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "project" DROP COLUMN "rewardStreamEnd"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "project" DROP COLUMN "rewardStreamStart"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "donation" DROP COLUMN "lockedRewardTokenAmount"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "donation" DROP COLUMN "rewardTokenAmount"`,
+    );
+  }
+}
