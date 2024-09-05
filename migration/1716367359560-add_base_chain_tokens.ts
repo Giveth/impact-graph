291c317e187b6d2bd2680d7ff145d520d601b532
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2024-09-05 15:52:52 +0330

    Fix add base token migration

--- migration/1716367359560-add_base_chain_tokens.ts
+++ migration/1716367359560-add_base_chain_tokens.ts
@@ -6,59 +6,46 @@ import { NETWORK_IDS } from '../src/provider';
 
 export class AddBaseChainTokens1716367359560 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    try {
-      const environment = config.get('ENVIRONMENT') as string;
+    const environment = config.get('ENVIRONMENT') as string;
 
-      const networkId =
-        environment === 'production'
-          ? NETWORK_IDS.BASE_MAINNET
-          : NETWORK_IDS.BASE_SEPOLIA;
+    const networkId =
+      environment === 'production'
+        ? NETWORK_IDS.BASE_MAINNET
+        : NETWORK_IDS.BASE_SEPOLIA;
 
-      try {
-        await queryRunner.manager.save(
-          Token,
-          seedTokens
-            .filter(token => token.networkId === networkId)
-            .map(token => {
-              const t = {
-                ...token,
-              };
-              t.address = t.address?.toLowerCase();
-              delete t.chainType;
-              return t;
-            }),
-        );
-      } catch (e) {
-        // eslint-disable-next-line no-console
-        console.log(
-          'Error in migration AddBaseChainTokens1716367359560, saving tokens',
-          e,
-        );
-      }
-      const tokens = await queryRunner.query(`
-              SELECT * FROM token
-              WHERE "networkId" = ${networkId}
-              `);
-      const givethOrganization = (
-        await queryRunner.query(`SELECT * FROM organization
-          WHERE label='giveth'`)
-      )[0];
+    await queryRunner.manager.save(
+      Token,
+      seedTokens
+        .filter(token => token.networkId === networkId)
+        .map(token => {
+          const t = {
+            ...token,
+          };
+          t.address = t.address?.toLowerCase();
+          delete t.chainType;
+          return t;
+        }),
+    );
+    const tokens = await queryRunner.query(`
+            SELECT * FROM token
+            WHERE "networkId" = ${networkId}
+            `);
+    const givethOrganization = (
+      await queryRunner.query(`SELECT * FROM organization
+        WHERE label='giveth'`)
+    )[0];
 
-      const traceOrganization = (
-        await queryRunner.query(`SELECT * FROM organization
-          WHERE label='trace'`)
-      )[0];
+    const traceOrganization = (
+      await queryRunner.query(`SELECT * FROM organization
+        WHERE label='trace'`)
+    )[0];
 
-      for (const token of tokens) {
-        // Add all Base tokens to Giveth organization
-        await queryRunner.query(`INSERT INTO organization_tokens_token ("tokenId","organizationId") VALUES
-          (${token.id}, ${givethOrganization.id}),
-          (${token.id}, ${traceOrganization.id})
-        ;`);
-      }
-    } catch (e) {
-      // eslint-disable-next-line no-console
-      console.log('Error in migration AddBaseChainTokens1716367359560', e);
+    for (const token of tokens) {
+      // Add all Base tokens to Giveth organization
+      await queryRunner.query(`INSERT INTO organization_tokens_token ("tokenId","organizationId") VALUES
+        (${token.id}, ${givethOrganization.id}),
+        (${token.id}, ${traceOrganization.id})
+      ;`);
     }
   }
 
