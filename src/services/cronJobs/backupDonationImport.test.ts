c87d719a19e60e6f64b278b38f3cda48d5fb0140
Amin Latifi <a.latifi.al@gmail.com>
2024-09-17 23:15:55 +0330

    Decomission backup donation service

--- src/services/cronJobs/backupDonationImport.test.ts
+++ src/services/cronJobs/backupDonationImport.test.ts
@@ -1,90 +1,90 @@
-import { assert } from 'chai';
-import { createBackupDonation } from './backupDonationImportJob';
-import {
-  assertThrowsAsync,
-  createProjectData,
-  generateRandomEtheriumAddress,
-  generateRandomEvmTxHash,
-  saveProjectDirectlyToDb,
-} from '../../../test/testUtils';
-import { User } from '../../entities/user';
-import { QACC_NETWORK_ID } from '../../provider';
-import { DONATION_STATUS } from '../../entities/donation';
-import { findTokenByNetworkAndSymbol } from '../../utils/tokenUtils';
-import { QACC_DONATION_TOKEN_SYMBOL } from '../../utils/qacc';
+// import { assert } from 'chai';
+// import { createBackupDonation } from './backupDonationImportJob';
+// import {
+//   assertThrowsAsync,
+//   createProjectData,
+//   generateRandomEtheriumAddress,
+//   generateRandomEvmTxHash,
+//   saveProjectDirectlyToDb,
+// } from '../../../test/testUtils';
+// import { User } from '../../entities/user';
+// import { QACC_NETWORK_ID } from '../../provider';
+// import { DONATION_STATUS } from '../../entities/donation';
+// import { findTokenByNetworkAndSymbol } from '../../utils/tokenUtils';
+// import { QACC_DONATION_TOKEN_SYMBOL } from '../../utils/qacc';
 
-describe('createBackupDonation test cases', createBackupDonationTestCases);
+// describe('createBackupDonation test cases', createBackupDonationTestCases);
 
-function createBackupDonationTestCases() {
-  it('should create donation successfully', async () => {
-    const project = await saveProjectDirectlyToDb(createProjectData());
-    const donorWalletAddress = generateRandomEtheriumAddress();
-    await User.create({
-      walletAddress: donorWalletAddress,
-      loginType: 'wallet',
-      firstName: 'first name',
-    }).save();
-    const token = await findTokenByNetworkAndSymbol(
-      QACC_NETWORK_ID,
-      QACC_DONATION_TOKEN_SYMBOL,
-    );
+// function createBackupDonationTestCases() {
+//   it('should create donation successfully', async () => {
+//     const project = await saveProjectDirectlyToDb(createProjectData());
+//     const donorWalletAddress = generateRandomEtheriumAddress();
+//     await User.create({
+//       walletAddress: donorWalletAddress,
+//       loginType: 'wallet',
+//       firstName: 'first name',
+//     }).save();
+//     const token = await findTokenByNetworkAndSymbol(
+//       QACC_NETWORK_ID,
+//       QACC_DONATION_TOKEN_SYMBOL,
+//     );
 
-    const donation = await createBackupDonation({
-      projectId: project.id,
-      chainId: QACC_NETWORK_ID,
-      txHash: generateRandomEvmTxHash(),
-      nonce: 1,
-      amount: 10,
-      _id: '65a90d86d3a1115b4ebc0731',
-      token: {
-        symbol: token.symbol,
-        address: token.address,
-        networkId: QACC_NETWORK_ID,
-      },
-      anonymous: false,
-      symbol: QACC_DONATION_TOKEN_SYMBOL,
-      walletAddress: donorWalletAddress,
-      imported: false,
-    });
-    assert.isOk(donation);
-    // assert.isTrue(donation?.isTokenEligibleForGivback);
-    assert.equal(donation.status, DONATION_STATUS.PENDING);
+//     const donation = await createBackupDonation({
+//       projectId: project.id,
+//       chainId: QACC_NETWORK_ID,
+//       txHash: generateRandomEvmTxHash(),
+//       nonce: 1,
+//       amount: 10,
+//       _id: '65a90d86d3a1115b4ebc0731',
+//       token: {
+//         symbol: token.symbol,
+//         address: token.address,
+//         networkId: QACC_NETWORK_ID,
+//       },
+//       anonymous: false,
+//       symbol: QACC_DONATION_TOKEN_SYMBOL,
+//       walletAddress: donorWalletAddress,
+//       imported: false,
+//     });
+//     assert.isOk(donation);
+//     // assert.isTrue(donation?.isTokenEligibleForGivback);
+//     assert.equal(donation.status, DONATION_STATUS.PENDING);
 
-    // should use input createdAt not now time
-    assert.equal(donation.createdAt.getTime(), 1705577862000);
-  });
+//     // should use input createdAt not now time
+//     assert.equal(donation.createdAt.getTime(), 1705577862000);
+//   });
 
-  it('should fail if projectId is invalid', async () => {
-    const donorWalletAddress = generateRandomEtheriumAddress();
-    await User.create({
-      walletAddress: donorWalletAddress,
-      loginType: 'wallet',
-      firstName: 'first name',
-    }).save();
-    const token = await findTokenByNetworkAndSymbol(
-      QACC_NETWORK_ID,
-      QACC_DONATION_TOKEN_SYMBOL,
-    );
+//   it('should fail if projectId is invalid', async () => {
+//     const donorWalletAddress = generateRandomEtheriumAddress();
+//     await User.create({
+//       walletAddress: donorWalletAddress,
+//       loginType: 'wallet',
+//       firstName: 'first name',
+//     }).save();
+//     const token = await findTokenByNetworkAndSymbol(
+//       QACC_NETWORK_ID,
+//       QACC_DONATION_TOKEN_SYMBOL,
+//     );
 
-    const badFunc = async () => {
-      await createBackupDonation({
-        projectId: 99999999,
-        chainId: QACC_NETWORK_ID,
-        txHash: generateRandomEvmTxHash(),
-        nonce: 1,
-        amount: 10,
-        _id: '65a90d86d3a1115b4ebc0731',
-        token: {
-          symbol: token.symbol,
-          address: token.address,
-          networkId: QACC_NETWORK_ID,
-        },
-        anonymous: false,
-        symbol: QACC_DONATION_TOKEN_SYMBOL,
-        walletAddress: donorWalletAddress,
-        imported: false,
-      });
-    };
-    await assertThrowsAsync(badFunc, 'Project not found.');
-  });
-}
+//     const badFunc = async () => {
+//       await createBackupDonation({
+//         projectId: 99999999,
+//         chainId: QACC_NETWORK_ID,
+//         txHash: generateRandomEvmTxHash(),
+//         nonce: 1,
+//         amount: 10,
+//         _id: '65a90d86d3a1115b4ebc0731',
+//         token: {
+//           symbol: token.symbol,
+//           address: token.address,
+//           networkId: QACC_NETWORK_ID,
+//         },
+//         anonymous: false,
+//         symbol: QACC_DONATION_TOKEN_SYMBOL,
+//         walletAddress: donorWalletAddress,
+//         imported: false,
+//       });
+//     };
+//     await assertThrowsAsync(badFunc, 'Project not found.');
+//   });
+// }
