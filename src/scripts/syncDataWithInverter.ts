cd350c53a25829782c986c8db424a082940dd44f
ali ebrahimi <aliebrahimi2079@gmail.com>
2024-09-18 13:58:59 +0330

    change logger to console

--- src/scripts/syncDataWithInverter.ts
+++ src/scripts/syncDataWithInverter.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import _ from 'lodash';
 import { ethers } from 'ethers';
 import { FindOptionsWhere } from 'typeorm';
@@ -8,11 +9,9 @@ import {
   StreamingPaymentProcessorResponse,
   Vesting,
 } from '../adapters/inverter/inverterAdapter';
-import { logger } from '../utils/logger';
 import { AppDataSource } from '../orm';
 import { getProvider, QACC_NETWORK_ID } from '../provider';
 
-// todo: check if we should use same network for inverter or not
 const adapter = new InverterAdapter(getProvider(QACC_NETWORK_ID));
 
 async function updateTokenPriceAndTotalSupplyForProjects(
@@ -23,19 +22,19 @@ async function updateTokenPriceAndTotalSupplyForProjects(
   const allProjects = await projectRepository.find({ where: projectFilter });
   for (const project of allProjects) {
     if (!project.abc) {
-      logger.error(
+      console.error(
         `sync project token price failed. project ${project.id} don't have abc object!`,
       );
       continue;
     }
     if (!project.abc.orchestratorAddress) {
-      logger.error(
+      console.error(
         `sync project token price failed. can not find orchestratorAddress for project ${project.id}!`,
       );
       continue;
     }
     try {
-      logger.debug(
+      console.debug(
         `start fetching token price and total supply of project ${project.id}`,
       );
       const price = await fetchTokenPrice(project);
@@ -47,11 +46,11 @@ async function updateTokenPriceAndTotalSupplyForProjects(
         project.abc.totalSupply = totalSupply;
       }
       await project.save();
-      logger.debug(
+      console.debug(
         `token price and total supply of project ${project.id} saved successfully`,
       );
     } catch (error) {
-      logger.error(
+      console.error(
         `Error in update token price and total supply of project ${project.id}`,
         error,
       );
@@ -61,14 +60,14 @@ async function updateTokenPriceAndTotalSupplyForProjects(
 
 async function fetchTokenPrice(project: Project) {
   try {
-    logger.debug(`start fetching token price for project ${project.id}:`);
+    console.debug(`start fetching token price for project ${project.id}:`);
     const tokenPrice = await adapter.getTokenPrice(
       project.abc.fundingManagerAddress,
     );
-    logger.debug(`Fetched token price for project ${project.id}:`, tokenPrice);
+    console.debug(`Fetched token price for project ${project.id}:`, tokenPrice);
     return parseFloat(tokenPrice);
   } catch (error) {
-    logger.error(`Error in fetch token price of project ${project.id}`, error);
+    console.error(`Error in fetch token price of project ${project.id}`, error);
     return;
   }
 }
@@ -78,13 +77,13 @@ async function fetchTokenTotalSupply(project: Project) {
     const tokenTotalSupply = await adapter.getTokenTotalSupplyByAddress(
       project.abc.orchestratorAddress,
     );
-    logger.debug(
+    console.debug(
       `Fetched total supply for project ${project.id}:`,
       tokenTotalSupply,
     );
     return parseFloat(tokenTotalSupply);
   } catch (error) {
-    logger.error(
+    console.error(
       `Error fetching total supply for project ${project.id}:`,
       error,
     );
@@ -110,67 +109,67 @@ async function updateRewardsForDonations(
     const donationsByProjectId = _.groupBy(donations, 'projectId');
 
     for (const projectId of Object.keys(donationsByProjectId)) {
-      logger.debug(
+      console.debug(
         `Start fetching reward data for project ${projectId} donations`,
       );
       await fillRewardDataOfProjectDonations(donationsByProjectId[projectId]);
-      logger.debug(`Reward data filled for project ${projectId} donations`);
+      console.debug(`Reward data filled for project ${projectId} donations`);
     }
   } catch (error) {
-    logger.error(`Error updating rewards for donations`, error);
+    console.error(`Error updating rewards for donations`, error);
   }
 }
 
 async function fillRewardDataOfProjectDonations(donations: Donation[]) {
   const project = donations[0].project;
   if (!project.abc) {
-    logger.error(
+    console.error(
       `fill reward data of project donations failed. project ${project.id} don't have abc object!`,
     );
     return;
   }
   if (!project.abc.orchestratorAddress) {
-    logger.error(
+    console.error(
       `fill reward data of project donations failed. can not find orchestratorAddress for project ${project.id}!`,
     );
     return;
   }
   try {
-    logger.debug(
+    console.debug(
       `start fetching reward info from inverter for project ${project.id}`,
     );
     const rewardInfo: StreamingPaymentProcessorResponse =
       await adapter.getProjectRewardInfo(project.abc.orchestratorAddress);
-    logger.debug(`reward info for project ${project.id} fetched.`);
-    const rewards: Vesting[] = rewardInfo[0].vestings;
+    console.debug(`reward info for project ${project.id} fetched.`);
+    const rewards: Vesting[] = rewardInfo[0]?.vestings || [];
     for (const donation of donations) {
       const filteredRewards = rewards.filter(
         reward => reward.recipient === donation.fromWalletAddress,
       );
       if (filteredRewards.length === 0) {
-        logger.error(`no reward data exist for donation ${donation.id}!`);
+        console.error(`no reward data exist for donation ${donation.id}!`);
         continue;
       }
       if (!donation.blockNumber) {
-        logger.error(
+        console.error(
           `donation blockNumber not exist for donation ${donation.id}!`,
         );
         continue;
       }
-      logger.debug(
+      console.debug(
         `start getting block timestamp for block number: ${donation.blockNumber}, from network with Id: ${QACC_NETWORK_ID}`,
       );
       const donationBlockTimestamp = await adapter.getBlockTimestamp(
         donation.blockNumber,
       );
-      logger.debug(
+      console.debug(
         `the block timestamp for block number: ${donation.blockNumber} is: ${donationBlockTimestamp}`,
       );
       const donationRewardInfo = filteredRewards.filter(
         reward => reward.blockTimestamp === donationBlockTimestamp,
       );
       if (donationRewardInfo.length === 0) {
-        logger.error(
+        console.error(
           `donation blockTimestamp for donation ${donation.id} did not match any reward data blockTimes!
           donationBlockTimestamp = ${donationBlockTimestamp}`,
         );
@@ -178,7 +177,7 @@ async function fillRewardDataOfProjectDonations(donations: Donation[]) {
       }
       let reward = donationRewardInfo[0];
       if (donationRewardInfo.length > 1) {
-        logger.debug(
+        console.debug(
           `find more that one reward info for user ${donation.userId} in one block!`,
         );
         const userDonationsInThisBlock = donations.filter(
@@ -187,7 +186,7 @@ async function fillRewardDataOfProjectDonations(donations: Donation[]) {
             d.blockNumber === donation.blockNumber,
         );
         if (userDonationsInThisBlock.length !== donationRewardInfo.length) {
-          logger.error(
+          console.error(
             `the number of user donations in the ${donation.blockNumber} block is ${userDonationsInThisBlock.length}
              but we have ${donationRewardInfo.length} reward info for it!`,
           );
@@ -204,7 +203,7 @@ async function fillRewardDataOfProjectDonations(donations: Donation[]) {
         );
         reward = sortedRewardInfo[currentDonationIndex];
       }
-      logger.debug(`donation reward data for donation: ${donation.id}, is: 
+      console.debug(`donation reward data for donation: ${donation.id}, is: 
       ${reward.start}, ${reward.end}, ${reward.cliff}, ${reward.amountRaw}`);
 
       donation.rewardStreamStart = new Date(parseInt(reward.start));
@@ -215,12 +214,12 @@ async function fillRewardDataOfProjectDonations(donations: Donation[]) {
       donation.cliff = parseFloat(reward.cliff);
 
       await donation.save();
-      logger.debug(
+      console.debug(
         `reward data of donation ${donation.id} filled successfully.`,
       );
     }
   } catch (error) {
-    logger.error(`fill reward data of project donations failed!`, error);
+    console.error(`fill reward data of project donations failed!`, error);
     return;
   }
 }
@@ -237,9 +236,9 @@ export async function syncDonationsWithBlockchainData(
     donationFilter: {},
   },
 ) {
-  logger.debug('bootstrap() before AppDataSource.initialize()', new Date());
+  console.debug('bootstrap() before AppDataSource.initialize()', new Date());
   await AppDataSource.initialize(false);
-  logger.debug('bootstrap() after AppDataSource.initialize()', new Date());
+  console.debug('bootstrap() after AppDataSource.initialize()', new Date());
 
   await updateTokenPriceAndTotalSupplyForProjects(projectFilter);
 
