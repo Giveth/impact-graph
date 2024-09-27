014dc02cc224355140496c80c8352cb9b6a36d66
ali ebrahimi <aliebrahimi2079@gmail.com>
2024-09-27 14:30:04 +0330

    update migration to make new fields nullable

--- migration/1727434746651-addRoundCaps.ts
+++ migration/1727434746651-addRoundCaps.ts
@@ -1,26 +1,26 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AddRoundCaps1727432388159 implements MigrationInterface {
-  name = 'AddRoundCaps1727432388159';
+export class AddRoundCaps1727434746651 implements MigrationInterface {
+  name = 'AddRoundCaps1727434746651';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "qf_round" ADD "roundUSDCapPerProject" integer NOT NULL`,
+      `ALTER TABLE "qf_round" ADD "roundUSDCapPerProject" integer`,
     );
     await queryRunner.query(
-      `ALTER TABLE "qf_round" ADD "roundUSDCapPerUserPerProject" integer NOT NULL`,
+      `ALTER TABLE "qf_round" ADD "roundUSDCapPerUserPerProject" integer`,
     );
     await queryRunner.query(
-      `ALTER TABLE "qf_round" ADD "POLPriceAtRoundStart" numeric(18,8) NOT NULL`,
+      `ALTER TABLE "qf_round" ADD "POLPriceAtRoundStart" numeric(18,8)`,
     );
     await queryRunner.query(
-      `ALTER TABLE "early_access_round" ADD "roundUSDCapPerProject" integer NOT NULL`,
+      `ALTER TABLE "early_access_round" ADD "roundUSDCapPerProject" integer`,
     );
     await queryRunner.query(
-      `ALTER TABLE "early_access_round" ADD "roundUSDCapPerUserPerProject" integer NOT NULL`,
+      `ALTER TABLE "early_access_round" ADD "roundUSDCapPerUserPerProject" integer`,
     );
     await queryRunner.query(
-      `ALTER TABLE "early_access_round" ADD "POLPriceAtRoundStart" numeric(18,8) NOT NULL`,
+      `ALTER TABLE "early_access_round" ADD "POLPriceAtRoundStart" numeric(18,8)`,
     );
   }
 
