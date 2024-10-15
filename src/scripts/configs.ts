e1f70bf7232ed0be1ec31d2636ef420940b86ff6
ali ebrahimi <aliebrahimi2079@gmail.com>
2024-10-15 04:07:43 +0330

    change funding pot repo url to inverter repo

--- src/scripts/configs.ts
+++ src/scripts/configs.ts
@@ -3,7 +3,7 @@ import path from 'path';
 // Path to the local reports directory inside the repo
 export const reportsDir = path.join(__dirname, 'reportFiles/output');
 // The URL of the GitHub repository containing the reports
-export const repoUrl = 'https://github.com/ae2079/funding-pot.git';
+export const repoUrl = 'https://github.com/InverterNetwork/funding-pot.git';
 // Local directory for cloning or pulling the latest reports
 export const repoLocalDir = path.join(__dirname, '/funding-pot-repo');
 // Subdirectory inside the repo where reports are located
