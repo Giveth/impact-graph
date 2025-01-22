c1d6c9459fc3aed9f3d6524f1dd376d41a7cf77f
Carlos <carlos.quintero096@gmail.com>
2025-01-22 12:29:25 +0100

    add cluster matching sync timestamp and logs

--- src/entities/qfRound.ts
+++ src/entities/qfRound.ts
@@ -126,6 +126,10 @@ export class QfRound extends BaseEntity {
   @Column({ default: false })
   isDataAnalysisDone: boolean;
 
+  @Field(_type => Date)
+  @Column({ nullable: true })
+  clusterMatchingSyncAt: Date;
+
   @UpdateDateColumn()
   updatedAt: Date;
 
--- src/services/cronJobs/syncEstimatedClusterMatchingJob.ts
+++ src/services/cronJobs/syncEstimatedClusterMatchingJob.ts
@@ -56,6 +56,10 @@ export const fetchAndUpdateClusterEstimatedMatching = async () => {
       activeQfRound.id,
       matchingData,
     );
+
+    // Update latest job ran succesfully
+    activeQfRound.clusterMatchingSyncAt = new Date();
+    await activeQfRound.save();
   } catch (e) {
     logger.error('fetchAndUpdateClusterEstimatedMatching error', e);
   } finally {
