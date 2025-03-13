1b19808fd00df1a31b14e4a967a24bf59f6bf3ac
ali <aliebrahimi2079@gmail.com>
2025-03-14 00:55:37 +0330

    Update Polygon token references to use 'POL' instead of 'MATIC' in provider and constants files

--- src/constants/qacc.ts
+++ src/constants/qacc.ts
@@ -2,16 +2,17 @@ import config from '../config';
 
 export const QACC_DONATION_TOKEN_ADDRESS: string = (
   (config.get('QACC_DONATION_TOKEN_ADDRESS') as string) ||
-  //https://zkevm.polygonscan.com/token/0x22B21BedDef74FE62F031D2c5c8F7a9F8a4b304D#readContract
-  '0x22B21BedDef74FE62F031D2c5c8F7a9F8a4b304D'
+  // POL token is the native token of Polygon
+  '0x0000000000000000000000000000000000000000'
 ).toLowerCase();
 export const QACC_DONATION_TOKEN_SYMBOL =
-  (config.get('QACC_DONATION_TOKEN_SYMBOL') as string) || 'MATIC';
+  (config.get('QACC_DONATION_TOKEN_SYMBOL') as string) || 'POL';
 export const QACC_DONATION_TOKEN_NAME =
-  (config.get('QACC_DONATION_TOKEN_NAME') as string) || 'Matic token';
+  (config.get('QACC_DONATION_TOKEN_NAME') as string) || 'POL token';
 export const QACC_DONATION_TOKEN_DECIMALS =
   (+config.get('QACC_DONATION_TOKEN_DECIMALS') as number) || 18;
 export const QACC_DONATION_TOKEN_COINGECKO_ID =
-  (config.get('QACC_DONATION_TOKEN_COINGECKO_ID') as string) || 'matic-network';
+  (config.get('QACC_DONATION_TOKEN_COINGECKO_ID') as string) ||
+  'polygon-ecosystem-token';
 export const QACC_PRICE_FETCH_LEAD_TIME_IN_SECONDS =
   (+config.get('QACC_PRICE_FETCH_LEAD_TIME_IN_SECONDS') as number) || 300; // 5 minutes
