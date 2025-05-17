0ead40fdfa6ac39b1ca72f4d936458dc1e31fd8b
kkatusic <kkatusic@gmail.com>
2025-05-17 19:04:15 +0200

    try to add sleep

--- src/adapters/givpowerSubgraph/givPowerSubgraphAdapter.test.ts
+++ src/adapters/givpowerSubgraph/givPowerSubgraphAdapter.test.ts
@@ -2,7 +2,7 @@ import { assert } from 'chai';
 import { formatGivPowerBalance } from './givPowerSubgraphAdapter';
 import { generateRandomEtheriumAddress } from '../../../test/testUtils';
 import { givPowerSubgraphAdapter } from '../adaptersFactory';
-import { logger } from '../../utils/logger';
+
 describe(
   'getUserPowerBalanceInBlockNumber() test cases',
   getUserPowerBalanceInBlockNumberTestCases,
@@ -16,42 +16,22 @@ describe(
 
 function getUserPowerBalanceInBlockNumberTestCases() {
   it('should return correct info for block 24124422', async () => {
-    // const firstAddress = '0x00d18ca9782be1caef611017c2fbc1a39779a57c';
-    // const secondAddress = '0x05a1ff0a32bc24265bcb39499d0c5d9a6cb2011c';
-    // const fakeWalletAddress = generateRandomEtheriumAddress();
-    // const result =
-    //   await givPowerSubgraphAdapter.getUserPowerBalanceAtBlockNumber({
-    //     blockNumber: 24124422,
-    //     walletAddresses: [firstAddress, secondAddress, fakeWalletAddress],
-    //   });
-    // assert.equal(Object.keys(result).length, 3);
-    // assert.equal(result[firstAddress].balance, 127095.68);
-    // assert.equal(result[secondAddress].balance, 25000);
-    // assert.equal(result[fakeWalletAddress].balance, 0);
-    try {
-      const firstAddress = '0x00d18ca9782be1caef611017c2fbc1a39779a57c';
-      const secondAddress = '0x05a1ff0a32bc24265bcb39499d0c5d9a6cb2011c';
-      const fakeWalletAddress = generateRandomEtheriumAddress();
-      const result =
-        await givPowerSubgraphAdapter.getUserPowerBalanceAtBlockNumber({
-          blockNumber: 24124422,
-          walletAddresses: [firstAddress, secondAddress, fakeWalletAddress],
-        });
-
-      assert.equal(Object.keys(result).length, 3);
-      assert.equal(result[firstAddress].balance, 127095.68);
-      assert.equal(result[secondAddress].balance, 25000);
-      assert.equal(result[fakeWalletAddress].balance, 0);
-    } catch (err: any) {
-      logger.warn('Skipped due to rate limit (429):', err);
-      if (err.response?.status === 429) {
-        logger.warn('Skipped due to rate limit (429):', err.message);
-        return; // gracefully exit the test
-      }
-      throw err; // rethrow others
-    }
+    await new Promise(r => setTimeout(r, Math.random() * 3000));
+    const firstAddress = '0x00d18ca9782be1caef611017c2fbc1a39779a57c';
+    const secondAddress = '0x05a1ff0a32bc24265bcb39499d0c5d9a6cb2011c';
+    const fakeWalletAddress = generateRandomEtheriumAddress();
+    const result =
+      await givPowerSubgraphAdapter.getUserPowerBalanceAtBlockNumber({
+        blockNumber: 24124422,
+        walletAddresses: [firstAddress, secondAddress, fakeWalletAddress],
+      });
+    assert.equal(Object.keys(result).length, 3);
+    assert.equal(result[firstAddress].balance, 127095.68);
+    assert.equal(result[secondAddress].balance, 25000);
+    assert.equal(result[fakeWalletAddress].balance, 0);
   });
   it('should return correct info for block 24344249', async () => {
+    await new Promise(r => setTimeout(r, Math.random() * 3000));
     const firstAddress = '0x00d18ca9782be1caef611017c2fbc1a39779a57c';
     const secondAddress = '0x05a1ff0a32bc24265bcb39499d0c5d9a6cb2011c';
     const fakeWalletAddress = generateRandomEtheriumAddress();
@@ -69,6 +49,7 @@ function getUserPowerBalanceInBlockNumberTestCases() {
 
 function getLatestIndexedBlockTestCases() {
   it('should fetch latest block info', async () => {
+    await new Promise(r => setTimeout(r, Math.random() * 3000));
     const block = await givPowerSubgraphAdapter.getLatestIndexedBlockInfo();
     assert.isOk(block);
     assert.isAbove(block.number, 0);
