c1f4624b1dcb35901229ade99e5a181ccfba47de
Carlos <carlos.quintero096@gmail.com>
2025-09-03 12:47:04 +0200

    fix db issue

--- src/entities/projectQfRound.ts
+++ src/entities/projectQfRound.ts
@@ -1,44 +1,31 @@
 import { Field, ID, ObjectType } from 'type-graphql';
 import {
-  PrimaryGeneratedColumn,
-  Column,
+  PrimaryColumn,
   Entity,
   ManyToOne,
-  RelationId,
   BaseEntity,
   CreateDateColumn,
   UpdateDateColumn,
-  Index,
-  Unique,
 } from 'typeorm';
 import { Project } from './project';
 import { QfRound } from './qfRound';
 
 @Entity('project_qf_rounds_qf_round')
 @ObjectType()
-@Unique(['projectId', 'qfRoundId'])
 export class ProjectQfRound extends BaseEntity {
   @Field(_type => ID)
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryColumn()
+  projectId: number;
+
+  @Field(_type => ID)
+  @PrimaryColumn()
+  qfRoundId: number;
 
   @ManyToOne(_type => Project, project => project.projectQfRoundRelations)
   project: Project;
 
-  @Index()
-  @Field(_type => ID)
-  @RelationId((projectQfRound: ProjectQfRound) => projectQfRound.project)
-  @Column()
-  projectId: number;
-
   @ManyToOne(_type => QfRound, qfRound => qfRound.projectQfRoundRelations)
   qfRound: QfRound;
-
-  @Index()
-  @Field(_type => ID)
-  @RelationId((projectQfRound: ProjectQfRound) => projectQfRound.qfRound)
-  @Column()
-  qfRoundId: number;
 
   @CreateDateColumn()
   createdAt: Date;
