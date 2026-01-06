11d5e5cc041daf5bed7d98ffa2dd6d699c36fccb
Ramin <raminramazanpour@gmail.com>
2026-01-06 18:25:46 +0330

    Disable access and visibility for QF Round actions in admin panel

--- src/server/adminJs/tabs/qfRoundTab.ts
+++ src/server/adminJs/tabs/qfRoundTab.ts
@@ -1,36 +1,36 @@
 import fs from 'fs';
+import adminJs, { ValidationError } from 'adminjs';
 import {
   ActionResponse,
   After,
 } from 'adminjs/src/backend/actions/action.interface';
-import adminJs, { ValidationError } from 'adminjs';
 import { RecordJSON } from 'adminjs/src/frontend/interfaces/record-json.interface';
+import config from '../../../config';
 import { QfRound } from '../../../entities/qfRound';
-import { canAccessQfRoundAction, ResourceActions } from '../adminJsPermissions';
+import { pinFile } from '../../../middleware/pinataUtils';
+import { AppDataSource } from '../../../ormconfig';
+import { NETWORK_IDS } from '../../../provider';
+import {
+  countActiveQfRounds,
+  findQfRoundById,
+  getRelatedProjectsOfQfRound,
+} from '../../../repositories/qfRoundRepository';
+import { relateManyProjectsToQfRound } from '../../../repositories/qfRoundRepository2';
+import { addQfRoundDonationsSheetToSpreadsheet } from '../../../services/googleSheets';
 import {
   getQfRoundActualDonationDetails,
   refreshProjectActualMatchingView,
   refreshProjectEstimatedMatchingView,
 } from '../../../services/projectViewsService';
+import { isQfRoundHasEnded } from '../../../services/qfRoundService';
+import { errorMessages } from '../../../utils/errorMessages';
+import { logger } from '../../../utils/logger';
+import { messages } from '../../../utils/messages';
 import {
   AdminJsContextInterface,
   AdminJsRequestInterface,
 } from '../adminJs-types';
-import { isQfRoundHasEnded } from '../../../services/qfRoundService';
-import {
-  findQfRoundById,
-  getRelatedProjectsOfQfRound,
-} from '../../../repositories/qfRoundRepository';
-import { NETWORK_IDS } from '../../../provider';
-import { logger } from '../../../utils/logger';
-import { messages } from '../../../utils/messages';
-import { addQfRoundDonationsSheetToSpreadsheet } from '../../../services/googleSheets';
-import { errorMessages } from '../../../utils/errorMessages';
-import { relateManyProjectsToQfRound } from '../../../repositories/qfRoundRepository2';
-import { pinFile } from '../../../middleware/pinataUtils';
-import { AppDataSource } from '../../../ormconfig';
-import { countActiveQfRounds } from '../../../repositories/qfRoundRepository';
-import config from '../../../config';
+import { canAccessQfRoundAction, ResourceActions } from '../adminJsPermissions';
 
 let initialProjectIds: number[] = [];
 
@@ -488,8 +488,8 @@ export const qfRoundTab = {
         isVisible: false,
       },
       new: {
-        isAccessible: ({ currentAdmin }) =>
-          canAccessQfRoundAction({ currentAdmin }, ResourceActions.NEW),
+        isAccessible: false, // Disabled - QF Rounds are now managed in v6-core admin panel
+        isVisible: false,
         handler: async (
           request: AdminJsRequestInterface,
           _response,
@@ -590,8 +590,8 @@ export const qfRoundTab = {
         after: fillProjects,
       },
       edit: {
-        isAccessible: ({ currentAdmin }) =>
-          canAccessQfRoundAction({ currentAdmin }, ResourceActions.EDIT),
+        isAccessible: false, // Disabled - QF Rounds are now managed in v6-core admin panel
+        isVisible: false,
         handler: async (
           request: AdminJsRequestInterface,
           response,
